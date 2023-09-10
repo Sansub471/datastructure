c1d374969d9c9d4927c01a56e96894d9e5677c07
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-10 07:21:43 +0545

    Introduction to pointers.

---
+++ pointer/lesson1_intro.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int a;
+    int *p;
+    p = &a;
+    a = 5;
+    printf("'p' has address     : %p\n", p);
+    printf("The address of a is : %p\n", &a);
+    printf("The address of p is : %p\n", &p);
+    printf("The value of p is   : %d\n", *p);
+
+    printf("The value of 'a' is : %d\n", a);
+    *p = 77;
+    // *p derefences the pointer, which means it accesses the memory location
+    // pointed to by 'p' (which is the memory location of 'a'), and sets 
+    // its value to '77'.
+    printf("The value of 'a' is : %d\n", a);
+    return 0;
+}
