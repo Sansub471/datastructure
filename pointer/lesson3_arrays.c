eea007f3f6da39ed5a069e9de594b9173d4e0fbd
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-11 23:56:28 +0545

    why is pointer strongly typed.

--- pointer/lesson3_arrays.c
+++ pointer/lesson3_arrays.c
@@ -6,5 +6,16 @@ int main(){
     p= &a;
     printf("Size of integer is %lu bytes \n", sizeof(int));
     printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)p, *p);
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p+1), (*p+1));
+
+    char* p0;
+    p0 = (char*)p; // typecasting
+    printf("Size of char is %lu bytes \n", sizeof(char));
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)p0, *p0);
+    // p is now typecasted to char pointer, hence it only accesses one byte
+    // of memory
+    // 1025 = 00000000 00000000 00000100 00000001
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p0+1), (*p0));
+
     return 0;
 }
