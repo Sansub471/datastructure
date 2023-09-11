e1fc20d88bd64e51f54861f7c1b359c2ad025c6a
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-11 23:58:50 +0545

    void pointer, next is pointer to pointer.

--- pointer/lesson3_arrays.c
+++ pointer/lesson3_arrays.c
@@ -6,7 +6,7 @@ int main(){
     p= &a;
     printf("Size of integer is %lu bytes \n", sizeof(int));
     printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)p, *p);
-    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p+1), (*p+1));
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p+1), *(p+1));
 
     char* p0;
     p0 = (char*)p; // typecasting
@@ -15,7 +15,11 @@ int main(){
     // p is now typecasted to char pointer, hence it only accesses one byte
     // of memory
     // 1025 = 00000000 00000000 00000100 00000001
-    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p0+1), (*p0));
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p0+1), *(p0+1));
 
+    // generic pointer type
+    void* p1;
+    p1 = p;
+    
     return 0;
 }
