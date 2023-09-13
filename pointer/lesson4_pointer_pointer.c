807cc4990a6ce6d306db81f5687fdd5d9603fc96
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-13 23:08:57 +0545

    array portion removed to new file.

--- pointer/lesson4_pointer_pointer.c
+++ pointer/lesson4_pointer_pointer.c
@@ -1,32 +1,8 @@
 // pointer to pointer concept
 #include<stdio.h>
 #include<stdlib.h>
-
-#define MAXSIZE  1
 int main(){
-    int* arr = malloc(MAXSIZE * sizeof(int));
-    int* arr_ptr = arr; // keep track of the head
-    if (arr == NULL){
-        printf("Memory allocation failed.");
-        return -1;
-    }
-    printf("Give me %d numbers : \n", MAXSIZE);
-    for (int i=0; i< MAXSIZE; i++){
-        scanf("%d", arr);
-        arr++;
-    }
 
-    // resetting the head pointer
-    arr = arr_ptr;
-    printf("The numbers entered are : \n");
-    for (int i=0; i< MAXSIZE; i++){
-        printf("%d \t", *arr);
-        arr++;
-    }
-    printf("\n");
-    free(arr_ptr); // free the memory to prevent memory leak
-
-    printf("\n");
     // pointer to pointers
     int x = 17;
     int* p = &x;
