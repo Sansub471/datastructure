f1a375d795e0ac052c30de604e072e2f22d61c4f
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-14 22:54:17 +0545

    lesson3 moved to lesson2 working.

--- pointer/lesson2_working.c
+++ pointer/lesson2_working.c
@@ -16,5 +16,27 @@ int main(){
     printf("Address p+2 is HEX : %p \t INT : %ld\n", p+2, (long int)(__intptr_t)(p+2));
     // integer takes four bytes, look the difference in address of p, p+1 and p+2
     printf("Can access memory location using pointer arithematic!\n");
+
+    // lesson 2
+    a = 1025;
+    p= &a;
+    printf("Size of integer is %lu bytes \n", sizeof(int));
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)p, *p);
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p+1), *(p+1));
+
+    char* p0;
+    p0 = (char*)p; // typecasting
+    printf("Size of char is %lu bytes \n", sizeof(char));
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)p0, *p0);
+    // p is now typecasted to char pointer, hence it only accesses one byte
+    // of memory
+    // 1025 = 00000000 00000000 00000100 00000001
+    printf("Address = %ld, value  = %d\n", (long int)(__intptr_t)(p0+1), *(p0+1));
+
+    // generic pointer type
+    void* p1;
+    p1 = p;
+    
+
     return 0;
 }
