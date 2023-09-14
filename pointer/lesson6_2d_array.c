66c14dfd5c91ef33134fd9c40ccab848d6bdd91b
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-14 22:07:06 +0545

    kept in a function

--- pointer/lesson6_2d_array.c
+++ pointer/lesson6_2d_array.c
@@ -4,44 +4,21 @@
 
 #define MAX_ROWS 2
 #define MAX_COLS 3
-int main(){
-    int* arr =(int*)malloc(MAX_COLS * MAX_ROWS * sizeof(int));
-    int* arr_ptr = arr;
 
-    printf("Elements for %d x %d matrix? \n", MAX_ROWS, MAX_COLS);
-    for(int i=0; i< MAX_ROWS; i++){
-        printf("For Row %d\n", i);
-        for(int j=0; j<MAX_COLS; j++){
-            scanf("%d", arr);
-            arr++;
-        }
-    }
-
-    arr = arr_ptr; // resetting the header pointer.
-    printf("Elements are, %d x %d matrix: \n", MAX_ROWS, MAX_COLS);
-    for(int i=0; i< MAX_ROWS; i++){
-        for(int j=0; j<MAX_COLS; j++){
-            printf("%d\t", *arr);
-            arr++;
-        }
-        printf("\n");
-    }
-
-    free(arr_ptr);
-
-    // usign double pointer
+void Array2DdoublePointer(){
+ // usign double pointer
     int** arr2 = (int**)malloc(MAX_ROWS*sizeof(int*));
 
     if(arr2 == NULL){
         printf("Memory allocation failed.\n");
-        return 1;
+        return;
     }
 
     for(int i=0; i<MAX_ROWS; i++){
         arr2[i] = (int*)malloc(MAX_COLS * sizeof(int));
         if(arr2[i] == NULL){
             printf("Memory allocation failed.\n");
-            return 1;
+            return;
         }
     }
 
@@ -66,7 +43,42 @@ int main(){
         free(arr2[i]);
     }
     free(arr2);
+}
 
+void Array2DsinglePointer(){
+    int* arr =(int*)malloc(MAX_COLS * MAX_ROWS * sizeof(int));
+    if(arr == NULL){
+        printf("Memory allocation failed.\n");
+        return;
+    }
+    int* arr_ptr = arr;
+
+    printf("Elements for %d x %d matrix? \n", MAX_ROWS, MAX_COLS);
+    for(int i=0; i< MAX_ROWS; i++){
+        printf("For Row %d\n", i);
+        for(int j=0; j<MAX_COLS; j++){
+            scanf("%d", arr);
+            arr++;
+        }
+    }
+
+    arr = arr_ptr; // resetting the header pointer.
+    printf("Elements are, %d x %d matrix: \n", MAX_ROWS, MAX_COLS);
+    for(int i=0; i< MAX_ROWS; i++){
+        for(int j=0; j<MAX_COLS; j++){
+            printf("%d\t", *arr);
+            arr++;
+        }
+        printf("\n");
+    }
+
+    free(arr_ptr);
+}
+
+int main(){
+    
+    //Array2DsinglePointer();
+    Array2DdoublePointer();
     return 0;
 }
 
