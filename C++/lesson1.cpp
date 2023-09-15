97a257414b192851b62213a44bb2933066aef262
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-15 22:17:21 +0545

    revising C++ after long long time.

---
+++ C++/lesson1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+class Student
+{
+    private:
+        char* name;
+        char* SID;
+        char* address;
+        int grade;
+        float GPA;
+    public:
+};
+
+int main(){
+    std::cout<<"Nepal"<<std::endl;
+    return 0;
+}
