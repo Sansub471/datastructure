4ab6846826e4b7ab17eeae369ee41c28d7e170e9
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-15 15:55:06 +0545

    structure pointer completed.

--- pointer/lesson8_structure.c
+++ pointer/lesson8_structure.c
@@ -8,38 +8,31 @@ struct Student{
     float GPA;
 };
 
+struct Student* newStudent(char* name, int age, float GPA){
+    struct Student* s = (struct Student*)malloc(sizeof(struct Student));
+    s->name = name; // (*s).name = name;
+    s->age = age;
+    s->GPA = GPA;
+    return s;
+}
+
+void Print(struct Student* s){
+    printf("The student's profile : \n");
+    printf("Name : %s\n", s->name);
+    printf("Age  : %d\n", (*s).age); // s->age
+    printf("GPA  : %.2f\n", s->GPA);
+    printf("\n");
+}
 int main(){
     struct Student* s = (struct Student*)malloc(sizeof(struct Student));
     s->name = "Subash Mainali";
     s->age = 24;
     s->GPA = 3.78;
+    Print(s);
 
-    printf("The student's profile : \n");
-    printf("Name : %s\n", s->name);
-    printf("Age  : %d\n", (*s).age); // s->age
-    printf("GPA  : %.2f\n", s->GPA);
+    char* cricketPlayer = "Kushal Bhurtel";
+    struct Student* std = newStudent(cricketPlayer, 26, 3.89);
+    Print(std);
 
-    // string revision
-    char name[100] = "The name of my school is IOE.";
-    printf("The school is : %s \n", name);
-
-    char* school = "The school name is IOE.";
-    printf("The school : %s\n", school);
-    printf("The school : %c\n", school[19]);
-    
-    // looping over string using char*
-    printf("School name by looping : ");
-    while(*school != '\0'){
-        printf("%c", *school);
-        school++;
-    }
-    printf("\n");
-
-    // dynamic string memory allocation
-    char* dynamic_name = (char*)malloc(MAX_SIZE * sizeof(char));
-    printf("Enter a string : ");
-    scanf("%s", dynamic_name);
-
-    printf("The entered string is : %s\n", dynamic_name);
     return 0;
 }
