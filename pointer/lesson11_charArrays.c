efb821917b83a4cab56ce3b45b47e413a611b589
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-16 22:16:57 +0545

    arrays and pointers are different types that are used in similar manner.

--- pointer/lesson11_charArrays.c
+++ pointer/lesson11_charArrays.c
@@ -29,6 +29,53 @@ int main(){
     // end character must be explicit
     printf("Country name : %s\n", country);
 
+    // arrays and pointers are different types that are used in similar manner
+    char C1[6] = "Hello"; 
+    char* C2;
+    C2 = C1;
+    printf("C2[1] = %c\n", C2[1]);  
+    C2[0] = 'A';
+    printf("C2 = %s\n", C2);
 
+    int lenc2 = strlen(C2);
+    // The characters of C2 are : 
+    printf("The characters of C2 are : ");
+    for(int i=0; i<lenc2; i++){
+        printf("%c", C2[i]); // C2[i] is *(C2 + i)
+    }
+    printf("\n");
+
+    printf("The characters of C2 are : ");
+    for(int i=0; i<lenc2; i++){
+        printf("%c", *(C2 + i)); // C2[i] is *(C2 + i)
+    }
+    printf("\n");
+
+    printf("The characters of C2 are : ");
+    for(int i=0; i<lenc2; i++){
+        printf("%c", *C2); // C2[i] is *(C2 + i)
+        C2++;
+    }
+    printf("\n\n");
+
+    printf("The characters of C1 are : ");
+    for(int i=0; i<lenc2; i++){
+        printf("%c", *(C1 + i)); // C1[i] is *(C1 + i)
+    }
+    printf("\n");
+
+    printf("The characters of C1 are : ");
+    for(int i=0; i<lenc2; i++){
+        printf("%c", C1[i]); // C1[i] is *(C1 + i)
+    }
+    printf("\n");
+
+    // But, C1  is not equal to C2; C1 = C2; error
+    printf("The characters of C1 are : ");
+    for(int i=0; i<lenc2; i++){
+        printf("%c", *C1); // points to the first char, C1[0]
+        //C1++; error
+    }
+    printf("\n");
     return 0;
 }
