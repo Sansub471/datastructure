94aca9326c153263b80f6209a9ae8e136e249005
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-19 23:19:21 +0545

    3d array pointers

--- pointer/lesson13_multiDimArray2.c
+++ pointer/lesson13_multiDimArray2.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 
 int main(){
+    int C[3][2][2] = {
+        {
+            {2,5},{7,9}
+        },
+        {
+            {3,4},{6,1}
+        },
+        {
+            {0,8},{11,13}
+        }
+    };
+
+    int(*p)[2][2] = C;
+    printf("The address of C         HEX: %p\t %ld\n", C, (long int)(__intptr_t)C);
+    printf("The address of *C        HEX: %p\t %ld\n", B, (long int)(__intptr_t)B);
     return 0;
 }
