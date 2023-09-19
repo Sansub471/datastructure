d9a6b599ec914c0fb213952956c6018db31dff3e
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-19 23:57:05 +0545

    array passing function working.

--- pointer/lesson14_passing_arrays.c
+++ pointer/lesson14_passing_arrays.c
@@ -1,5 +1,20 @@
 #include<stdio.h>
 
+int OneDimArray(int A[]){
+    // int A[] is seen as int* by the compiler
+    return *(A+1); // just random
+
+}
+
+int TwoDimArray(int B[][3]){
+    // only first dimension is allowed to left empty
+    return *(*B + 1);
+}
+
+
+int ThreeDimArray(int C[][2][2]){
+    return ***C;
+}
 int main(){
     int A[5] = {1,2,3,4,5};
     int B[2][3] = {{4,5,7},{9,13,17}};
@@ -14,6 +29,8 @@ int main(){
             {0,8},{11,13}
         }
     };
-    
+    printf("OneDimArray Function   : %d\n", OneDimArray(A));
+    printf("TwoDimArray Function   : %d\n", TwoDimArray(B));
+    printf("ThreeDimArray Function : %d\n", ThreeDimArray(C));
     return 0;
 }
