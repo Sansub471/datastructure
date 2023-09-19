32d84f409f7f4ffc3b207f00e11475c62aa3d16e
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-19 23:39:44 +0545

    printing 3-D array is completed.

--- pointer/lesson13_multiDimArray2.c
+++ pointer/lesson13_multiDimArray2.c
@@ -21,5 +21,32 @@ int main(){
     printf("The address of **C       HEX: %p\t %ld\n\n", **C, (long int)(__intptr_t)**C); // int(*) pointer
 
     printf("The value of ***C     : %d\n", ***C);
+
+    int(*q)[2] = C[0]; // pointer to 2-D array of size 2x2
+    // *q is pointer to first element of first 1-D array pointed by q
+    // Here, pointer to array {2,5}
+
+    int(*r)[2] = C[1]; // pointer to 2-D array of size 2x2
+    int(*s)[2] = C[2]; // pointer to 2-D array of size 2x2
+
+    printf("The value of **q     : %d\n\n", **q);
+
+    int* t = C[0][0];
+    printf("The 3-D array is : \n");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<2; j++){
+            for(int k=0; k<2; k++){
+                printf("%d\t", *(*(*(C + i) + j) + k));
+                // C[i][j][k] = *(C[i][j] + k)
+                //            = *(*(C[i] + j) + k)
+                //            = *(*(*(C + i) + j) + k)
+            }
+            printf("|");
+        }
+        printf("\n");
+    }
+
+    
+
     return 0;
 }
