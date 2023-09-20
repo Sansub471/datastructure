eea4c23cf08a2bdf4aafa7469146269dc1c9e0d3
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-20 23:29:06 +0545

    memory allocation usig malloc.

--- pointer/lesson15_memory_pointers.c
+++ pointer/lesson15_memory_pointers.c
@@ -14,7 +14,7 @@ int main(){
     p = (int*)malloc(sizeof(int));
     *p = 23;
 
-    int* arr = (int*)malloc(10*sizeof(int));
+
     // memory for 10 integers array
     // contagious memory
     
@@ -24,5 +24,38 @@ int main(){
     // arr = new int[10];
     // delete[] arr;
 
+    // Lesson 2
+    // Functions to allocate a block of memory in C
+
+    // malloc - void* malloc(size_t size)
+    // size_t is unsigned int
+    // size in bytes
+
+    // calloc - void* calloc(size_t num, size_t size)
+    int* ptr = (int*)calloc(3, sizeof(int));
+
+    // malloc vs calloc
+    // malloc doens't intialize the bytes with any values, they have garbage
+    // calloc initializes the bytes with zero value
+
+    // realloc - void* realloc(void* ptr, size_t size)
+
+    // example of malloc
+    int n;
+    printf("Enter the size of an array : ");
+    scanf("%d", &n);
+    int* arr = (int*)malloc(n*sizeof(int));
+    int* arr_ptr = arr; // keep track of start here
+
+    for(int i=0; i<n; i++){
+        //arr[i] = i+1;
+        *(arr + i) = i+1;
+    }
+
+    for(int i= 0; i<n; i++){
+        printf("%d ", *arr); // arr[i]
+        arr++;
+    }
+    printf("\n");
     return 0;
 }
