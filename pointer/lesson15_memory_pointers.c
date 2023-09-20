97ced80a9f3ce52a4dc7ebe6472fb83f10e5da42
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-09-20 23:50:49 +0545

    malloc, calloc, and realloc.

--- pointer/lesson15_memory_pointers.c
+++ pointer/lesson15_memory_pointers.c
@@ -32,7 +32,7 @@ int main(){
     // size in bytes
 
     // calloc - void* calloc(size_t num, size_t size)
-    int* ptr = (int*)calloc(3, sizeof(int));
+    //int* ptr = (int*)calloc(3, sizeof(int));
 
     // malloc vs calloc
     // malloc doens't intialize the bytes with any values, they have garbage
@@ -53,9 +53,44 @@ int main(){
     }
 
     for(int i= 0; i<n; i++){
-        printf("%d ", *arr); // arr[i]
-        arr++;
+        printf("%d ", *arr_ptr); // arr[i]
+        arr_ptr++;
     }
     printf("\n");
+
+    // let's deallocate memory for arr
+    free(arr); 
+    // memory block pointed by arr* is now availabe for new allocation
+    arr = NULL; // After free, adjust pointer to NULL
+    // eventhough memory is freed, arr still has address of the memory block
+    // it can be exploited later 
+
+    // example using calloc
+    int* arr2 = (int*)calloc(n, sizeof(int));
+    int* arr2_ptr = arr2; // keep track of head
+
+     for(int i=0; i<n; i++){
+        //arr[i] = i+1;
+        *(arr2 + i) = i+2;
+    }
+    for(int i= 0; i<n; i++){
+        printf("%d ", *arr2_ptr); // arr[i]
+        arr2_ptr++;
+    }
+    printf("\n");
+
+    int* arr3 = (int*)realloc(arr2, 2*n*sizeof(int));
+    // how it works
+    // memory size can be increased or decreased
+
+    // if the size of the new block is greater than previous
+    // block, and it is possible to extend the previous block
+    // i.e. find continuous memory block to it, then previous block is extented
+
+    // otherwise, previous block is copied to the new location
+    printf("Previous address, arr2 : %ld \t New address, arr3 : %ld\n", (long int)(__intptr_t)arr2, (long int)(__intptr_t)arr3);
     return 0;
 }
+
+// malloc should have random value and calloc zero
+// but both had zero on my machine
