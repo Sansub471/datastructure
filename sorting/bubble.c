0656922d8aa34cf7ca51ea44c229dd295e369f38
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-11-21 12:41:52 +0545

    The bubble sort worked, writing C after a long time.

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -1,1 +1,33 @@
-// This will be the bubble sort code i'm writing.
+#include<stdio.h>
+#define MAX 10
+
+int* bubbleSort(int* A, int N){
+    for(int i=0; i < N; i++){
+        for(int j=i; j < N; j++){
+            if (A[i] > A[j]){
+                // swap
+                A[i] = A[i] + A[j];
+                A[j] = A[i] - A[j];
+                A[i] = A[i] - A[j];
+            }
+        }
+    }
+    return A;
+}
+
+void Print(int* A){
+    printf("The array is : ");
+    for (int i=0; i < MAX; i++){
+        printf("%d\t", A[i]);
+    }
+    printf("\n");
+
+}
+
+ int main(){
+    int A[10] = {89, 34, 1, 3, -15, 37, 12, 23, 21, 10};
+    Print(A);
+    int* B = bubbleSort(A, MAX);
+    Print(B);    
+    return 0;
+ }
