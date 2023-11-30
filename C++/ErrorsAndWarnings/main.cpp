94aad4aea292af884a56c526446ff73870fdf167
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-11-30 11:52:10 +0545

    Errors and its types.

---
+++ C++/ErrorsAndWarnings/main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+
+int main(int argc, char** argv){
+    std::cout<<"Hello World, this is C++"<<std::endl;
+
+    // Runtime error
+    int div = 7 / 0;
+    std::cout<<"The value is : "<<div<<std::endl;
+    return 0;
+}
--- C++/lesson1.cpp
+++ C++/lesson1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-int main(){
+int main(int argc, char** argv){
     for(int i=0; i<10; i++)
         std::cout<<"Hello World."<<std::endl;
     return 0;
