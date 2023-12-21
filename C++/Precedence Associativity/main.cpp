0301cd40ca4ddbb6a29d1928ca7be9bf9dc435bc
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-12-21 23:39:32 +0545

    The precedance and associativity lesson is completed.

--- C++/Precedence Associativity/main.cpp
+++ C++/Precedence Associativity/main.cpp
@@ -15,13 +15,21 @@ int main(){
 
     int result = a + b * c - d / e  - f + g ; 
     int result1 = b + d / f - a + g + c * e + g / f;
+    int result2 = a/b*c + d - e + f; // Associativity left to right
+    int result3 = a*b/c + d - e +f; // Associativity left to right
+
 
     // by precedance, * first then / and finally associativity left to right
 
     std::cout<<"The result is : " << result << std::endl;
     std::cout<<"The result one is : " << result1 << std::endl;
+    std::cout<<"The result two is : " << result2 << std::endl;
+    std::cout<<"The result three is : " << result3 << std::endl;
     return 0;
 }
 
 // Link : check this out to referesh your memory
 // https://en.cppreference.com/w/cpp/language/operator_precedence
+
+// Hint : use parenthesis at the right place to make things clear and save time
+// Use () to make things very clear for readibility.
