61b9bae2afcd1e7a827892d1505e515e94981943
Sansub471 <075bei039.subash@pcampus.edu.np>
2023-12-23 23:41:11 +0545

    Comment of the day.

--- C++/Manipulators/main.cpp
+++ C++/Manipulators/main.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iomanip>
 
 
 // input/ output manipulators.
@@ -7,12 +8,16 @@ int main()
     std::cout<< " Printing a new line ... "<<std::endl; // print a new line
 
     std::flush;
-    std::setw(); // setwidth e.g a Formatted Table
+    std::setw(15); // setwidth e.g a Formatted Table
     std::right; // right justification
     std::internal; // sign in left justified, data is right justified.
+    // -    45.0233
+    // -    902.023
 
     std::boolalpha; // control bool output format : 1/0 or true/ false
     std::noboolalpha; // disable boolalpha
     std::showpos; // show or hide the + sign for positive numbers.
     return 0;
 }
+
+// The comment of for the day to save the trend
