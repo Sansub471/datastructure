34840ae8510a8f5fcefd6d9559f7ccfe2d7c4c6e
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-01-10 22:33:22 +0545

    The game is on.

--- C++/Manipulators/main.cpp
+++ C++/Manipulators/main.cpp
@@ -112,7 +112,11 @@ int main()
     }
 
     { 
-        
+         std::cout << std::endl;
+    std::cout << "double_var in different bases : " << std::endl;
+    std::cout << "(dec) : " << std::dec << double_var << std::endl;
+    std::cout << "(hex) : " << std::hex << double_var << std::endl;
+    std::cout << "(oct) : " << std::oct << double_var << std::endl;
     }
     return 0;
 }
