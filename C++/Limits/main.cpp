26f3a64f9081ef5e44f5cf10da1a36526bc523d1
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-01-16 21:20:10 +0545

    Extra lines removed.

--- C++/Limits/main.cpp
+++ C++/Limits/main.cpp
@@ -3,14 +3,9 @@
 // Goal : Learn about numeric limit
 // #include<limits> library in C++
 
-
-// Back to C++ programmig
-
 int main()
 {
 
-    double name{980238.9089};
-    std::cout << "The double name : " << name << std::endl;
     return 0;
 }
 
