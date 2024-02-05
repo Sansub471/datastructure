ec57a1a4afe5ca2d8713ae63f6b67a949fd79b9b
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-05 14:56:05 +0545

    Limits methods explored for different data types.

--- C++/Limits/main.cpp
+++ C++/Limits/main.cpp
@@ -18,6 +18,32 @@
 
 int main()
 {
+    std::cout<< "The range for short is from " << std::numeric_limits<short>::min() << " to " 
+                << std::numeric_limits<short>::max() << std::endl;
+    std::cout<< "The range for unsigned short is from " << std::numeric_limits<unsigned short>::min() << " to " 
+                << std::numeric_limits<unsigned short>::max() << std::endl;
 
+    std::cout<< "The range for int is from " << std::numeric_limits<short>::min() << 
+                " to " << std::numeric_limits<int>::max() << std::endl;
+    std::cout<< "The range for unsigned int is from " << std::numeric_limits<unsigned int>::min() << " to " 
+                << std::numeric_limits<unsigned int>::max() << std::endl;
+
+    std::cout<< "The range for long is from " << std::numeric_limits<long>::min() << " to " 
+                << std::numeric_limits<long>::max() << std::endl; // equivalent to long int
+
+    std::cout<< "The range for long int is from " << std::numeric_limits<long int>::min() << " to " 
+                << std::numeric_limits<long int>::max() << std::endl;
+    
+    std::cout<< "The range for unsigned long int is from " << std::numeric_limits<unsigned long int>::min() << " to " 
+                << std::numeric_limits<unsigned long int>::max() << std::endl; // equivalent to unsigned long
+
+    std::cout<< "The range for unsigned int is from " << std::numeric_limits<unsigned int>::min() << " to " 
+                << std::numeric_limits<unsigned int>::max() << std::endl;
+
+    std::cout<< "The range for unsigned int is from " << std::numeric_limits<unsigned int>::min() << " to " 
+                << std::numeric_limits<unsigned int>::max() << std::endl;
+
+    std::cout<< "The range for unsigned int is from " << std::numeric_limits<unsigned int>::min() << " to " 
+                << std::numeric_limits<unsigned int>::max() << std::endl;
     return 0;
 }
