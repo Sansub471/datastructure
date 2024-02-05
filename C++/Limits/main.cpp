694fc0eb2b40e2503df859ff3fd4682973cb9778
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-05 14:42:08 +0545

    Limits for different data types.

--- C++/Limits/main.cpp
+++ C++/Limits/main.cpp
@@ -3,6 +3,19 @@
 // Goal : Learn about numeric limit
 // #include<limits> library in C++
 
+// Floating point : 
+// lowest               min             max
+// -3.40282e+38         0 1.17549e-38   3.40282e+38
+
+// Signed integer (this is short)
+// min              max
+// -32768           32767
+
+// For unsigned min is zero and max is the maximum for that data type
+
+#include<limits>
+
+
 int main()
 {
 
