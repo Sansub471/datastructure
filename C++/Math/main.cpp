6fe8394650a41a1fc23c499761a8bb3df87cac03
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-08 23:39:54 +0545

    Sin function

--- C++/Math/main.cpp
+++ C++/Math/main.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include<cmath>
 
-// Exploring some math functions
-
-
 int main()
 {
+
+    double sin90 = std::sin(90); // Radian
+    std::cout<<"The value of sin90 is : " << sin90 << std::endl;
     return 0;
 }
