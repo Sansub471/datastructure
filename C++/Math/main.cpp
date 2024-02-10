12175e4bd18a9522567b5f085345c1e2e44b7a93
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-10 21:24:33 +0545

    Maths functions.

--- C++/Math/main.cpp
+++ C++/Math/main.cpp
@@ -6,5 +6,14 @@ int main()
 
     double sin90 = std::sin(90); // Radian
     std::cout<<"The value of sin90 is : " << sin90 << std::endl;
+
+    double tan90 = std::tan(90);
+    std::cout<<"The value of tan(90) is : " << tan90 << std::endl;
+
+    double epow = std::exp(100);
+    std::cout<<"The value of exp(100) is : " << epow << std::endl;
+
+    double china = std::log(200);
+    std::cout<<"The log(200) is : " << china << std::endl;
     return 0;
 }
