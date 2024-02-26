bd4921477e2cc49ec2f80dd47cf7d472819519f6
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-02-26 23:29:31 +0545

    Revise a switch case statement.

---
+++ C++/SwitchCase/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+const int sharpner{20};
+const int eraser{30};
+const int pencil{40};
+
+int main(){
+
+int tool {pencil};
+
+switch (tool) {
+    case  sharpner:
+    {
+        std::cout<<"Sharpen your pencil."<<std::endl;
+    }
+    break;
+    case eraser:
+    {
+        std::cout<<"Correct your mistake."<<std::endl;
+    }
+    break;
+    case pencil:
+    {
+        std::cout<<"Keep writing."<<std::endl;
+    }
+    break;
+    default :
+    {
+        std::cout<<"Today is reading time."<<std::endl;
+    }
+}
+    
+return 0;
+}
