af7871a3f716f253089a993c5eaed936b4dc87be
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-01 23:41:04 +0545

    A new way of writing for loop, size_t

---
+++ C++/Notes/notes1.cpp
@@ -0,0 +1,14 @@
+// A note of interesting looking code for me
+// Test new codes that are interesting to me
+
+#include<iostream>
+int main(){
+    // for loop
+    // use size_t : a representation of some unsigned int for positive numbers [sizes]
+    const size_t COUNT = 10;
+    for(size_t i{0};i < COUNT; i++){
+        std::cout<<"I have a life to live, find a way to discipline yourself."<<std::endl;
+    }
+    std::cout<<"End of life lesson."<<std::endl;
+    return 0;
+}
