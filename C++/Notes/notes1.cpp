c56ca7a43628fcb6b321fb6c84e7b45e3b5bccf7
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-01 23:49:38 +0545

    A new way of printing array values.

--- C++/Notes/notes1.cpp
+++ C++/Notes/notes1.cpp
@@ -10,5 +10,13 @@ int main(){
         std::cout<<"I have a life to live, find a way to discipline yourself."<<std::endl;
     }
     std::cout<<"End of life lesson."<<std::endl;
+    std::cout<<"The size of size_t is : " << sizeof(size_t) << std::endl;
+
+    // Array declaratio: omit size
+    int class_sizes[] {10, 12, 15, 11, 18, 17};
+    std::cout<<"Let's look at the values of the array : " << std::endl;
+    for(auto value : class_sizes){
+        std::cout<< "value : " << value << std::endl;
+    }
     return 0;
 }
