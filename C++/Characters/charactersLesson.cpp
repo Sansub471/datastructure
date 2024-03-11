3a4418d137acdcd8ca5df3a8675ba29327f87c32
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-11 23:06:49 +0545

    Character array.

--- C++/Characters/charactersLesson.cpp
+++ C++/Characters/charactersLesson.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
-
-
 int main(){
+    // character array
+    char msg[5] {'H', 'e', 'l','l','o'};
+
+    // printing the array using looping
+    std::cout<<"The message is : ";
+    for(auto m : msg){
+        std::cout<< m ;
+    }
+    std::cout<<std::endl;
+    // This method works, but it's not a good idea to use it like this, 
+    // writing each character is not easy
     
     return 0;
 }
