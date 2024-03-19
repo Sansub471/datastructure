0f8328a34821f403ccd38d064fbc7268d8dc3b96
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-19 20:49:29 +0545

    Speciality of char*, can be intialized with string literal.

--- C++/Pointers/pointer1.cpp
+++ C++/Pointers/pointer1.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
 // This is the first class on pointers
+// Already familiar with pointer to other data types
+// Declaring pointer to char
 
 int main(){
+    // can use normal pointer to char like for double, int, float, and
+    // anything else really
+
+    char* pchar{nullptr};
+    char charval = 'A';
+    pchar = &charval;
+
+    std::cout<<"The value stored in pchar : " << *pchar << std::endl;
+
+    // This is how we normally think, but there is something special here
+    // We can use string literal, "String Literal" with char*
     
 
     return 0;
