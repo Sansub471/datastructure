dff2517ae7f50ce38596fcad1c4cfbb7f614a9a8
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-26 21:55:15 +0545

    The basic of the pointer and order of pointers is done.

--- revision/pointer_one.c
+++ revision/pointer_one.c
@@ -5,10 +5,24 @@ int main(){
     float* ptrone = &varone;
 
     printf("The address of varone is : %p\n", &varone);
-    printf("The value of varone is : %.3f\n", varone);
+    printf("The value  of  varone is : %.3f\n", varone);
 
     varone = 77.17;
-    printf("The value of varone is : %.3f\n", *ptrone);
+    printf("The address of ptrone is : %p\n", &ptrone);
+    printf("The value  of  ptrone is : %.3f\n", *ptrone);
+
+    float** ptrdouble = &ptrone;
+    printf("The address of ptrdouble is : %p\n", &ptrdouble);
+    printf("The value of *ptrdouble is &ptrone : %p\n", *ptrdouble);
+    printf("The value of **ptrdouble is varone or *ptrone : %.3f\n", **ptrdouble);
+
+    // You can try with triple pointer too.
+
+    float*** ptrtriple = &ptrdouble;
+    float**** ptrquadra = &ptrtriple;
+    printf("The value of ****ptrquadra is : %.3f\n", ****ptrquadra);
+
+    // perhaps we can have even higher pointer, but who is gonna try?? lol
 
     return 0;
 }
