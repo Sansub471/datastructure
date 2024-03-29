b3c3629d39c13d4df90e00b237cc7a8114d72265
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-29 22:23:32 +0545

    Done with singe int* pointer as well. The conversion formula is i*cols+j

--- revision/pointer_six_2dArray.c
+++ revision/pointer_six_2dArray.c
@@ -48,11 +48,52 @@ void TwoDArrayOutput(int** arr){
     }
 }
 
+void FreeMemory(int** arr2d){
+    unsigned short i = 0;
+    for(i=0; i < MAXROW; i++){
+        free(arr2d[i]);
+    }
+}
+
+// Do the same thing with single pointer
+
+int* newArray(){
+    int* arr = (int*)malloc(MAXROW * MAXCOL * sizeof(int));
+    if(arr==NULL){
+        return NULL;
+    }
+    return arr;
+}
+
+void singlePtr2DArrInput(int* arr){
+    unsigned short i,j;
+    printf("The 2-D array of size %d x %d : \n", MAXROW, MAXCOL);
+    for(i=0; i<MAXROW; i++){
+        printf("Element row %d : \n", i);
+        for(j=0; j<MAXCOL; j++){
+            printf("Element [%d][%d] : ", i,j);
+            scanf("%d", arr + i * MAXCOL + j);// 2D to 1D formula : i * MAXCOLS + j
+            // row_index * MAXCOLS + column_index
+        }
+    }
+}
+
+void singlePtrArrOutput(int* arr){
+    printf("The matrix of size %d x %d is : \n", MAXROW, MAXCOL);
+    unsigned short i,j;
+    for(i=0; i < MAXROW; i++){
+        for(j=0; j < MAXCOL; j++){
+            printf("%d\t", *(arr + i * MAXCOL + j));
+        }
+        printf("\n");
+    }
+}
+
 int main(){
     // Make sure to free the memory correctly.
     int** arr2d = new2DArray();
 
-    printf("The size of arr2d : %ld \n", sizeof(arr2d));
+    //printf("The size of arr2d : %ld \n", sizeof(arr2d));
     if (arr2d == NULL){
         printf("Memory allocation failed, exiting.");
         return -1;
@@ -62,10 +103,19 @@ int main(){
     TwoDArrayOutput(arr2d);
 
     // At last free the allocated memory
-    unsigned short i = 0;
-    for(i=0; i < MAXROW; i++){
-        free(arr2d[i]);
-    }
+    FreeMemory(arr2d);
     free(arr2d);
+
+    // Using the single int* pointer
+    int* arr = newArray();
+    if(arr==NULL){
+        printf("The memory allocation failed.\n");
+        return -1;
+    }
+    singlePtr2DArrInput(arr);
+    singlePtrArrOutput(arr);
+    // Free the allocated memory
+    free(arr);
+
     return 0;
 }
