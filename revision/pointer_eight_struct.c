a852a8d50b64d442097b4713d92913faa8f9e1cb
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-03-31 22:18:54 +0545

    The function to create a structure is done.

--- revision/pointer_eight_struct.c
+++ revision/pointer_eight_struct.c
@@ -17,6 +17,23 @@ void PrintStudent(struct Student s){
     printf("GPA : %.2f\n", s.GPA);
     printf("--------------------------------------------------\n");
 }
+
+struct Student* newStudent(char* name, int age, char* level, float GPA){
+    struct Student* stdnt = (struct Student*)malloc(sizeof(struct Student));
+    if(stdnt == NULL){
+        return NULL; // Memory allocation has failed.
+    }
+    stdnt->name = name;
+    (*stdnt).age = age; 
+    stdnt->level = level;
+    stdnt->GPA = GPA;
+
+    // return &stdnt is not allowed, because it is the address of the pointer, not the address pointed by it
+    // Also, it would mean we are trying to return the address of the local variable 'stdnt'
+    // And, the return type for it would be struct** where in our functin it's struct*
+    return stdnt;
+}
+
 int main(){
     struct Student st;
     st.name = "Kushal Bhurtel";
@@ -25,6 +42,10 @@ int main(){
     st.GPA = 3.89;
 
     PrintStudent(st);
+
+    // Structure with dynamic allocation
+
+    
 
     return 0;
 }
