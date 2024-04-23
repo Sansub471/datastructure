062445536e5dffab1cdc847e53f23f00bfce287b
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-04-23 20:11:57 +0545

    The functions is done, but not working as of now.

--- sorting/selection.c
+++ sorting/selection.c
@@ -2,9 +2,29 @@
 #include<stdio.h>
 #include"utilities.c"
 
+int* SelectionSort(int* A, unsigned int N){
+    unsigned int i,j, iMin;
+    int temp;
+    for(i=0; i< N - 1; i++){
+        iMin = i; // Assume the position of smallest element
+        for(j=i+1; j < N; j++){
+            if(A[j] > A[iMin]){
+                iMin = j;
+            }
+            // Swap part
+            temp = A[i];
+            A[i] = A[iMin];
+            A[iMin] = temp;            
+        }
+    }
+    return A;
+}
 
 int main(){
     unsigned int N = getSize();
     int* A = InputArray(N);
     PrintArray(A, N);
+
+    printf("The sorted array is : ");
+    PrintArray(SelectionSort(A, N), N);
 }
--- sorting/utilities.h
+++ sorting/utilities.h
@@ -1,7 +1,7 @@
 #ifndef UTILITIES
 #define UTILITIES
 unsigned int getSize(); // take array size input
-int* inputArray(unsigned int size); // take array input and return it
+int* InputArray(unsigned int size); // take array input and return it
 void PrintArray(int* A, unsigned int N); // print the given array
 
 #endif
