2c24eb8df5d243a0572f660c0d151d7134e95f03
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-04-24 11:55:20 +0545

    Selection sort done, swap part must be outside the inner loop.

--- sorting/selection.c
+++ sorting/selection.c
@@ -5,26 +5,46 @@
 int* SelectionSort(int* A, unsigned int N){
     unsigned int i,j, iMin;
     int temp;
-    for(i=0; i< N - 1; i++){
+    for(i=0; i < (N - 1); i++){
         iMin = i; // Assume the position of smallest element
         for(j=i+1; j < N; j++){
-            if(A[j] > A[iMin]){
+            if(A[j] < A[iMin]){
                 iMin = j;
             }
-            // Swap part
-            temp = A[i];
-            A[i] = A[iMin];
-            A[iMin] = temp;            
         }
+        // Smallest in ith iteration found.
+        // Swap part
+        temp = A[i];
+        A[i] = A[iMin];
+        A[iMin] = temp;            
     }
     return A;
 }
 
+
+
 int main(){
+
+    // Let's show one example without user input
+    int arr[] = {56,12,7,21,67,23,17,9,13};
+    unsigned int size = sizeof(arr) / sizeof(arr[0]);
+    printf("The given array is : \n");
+    PrintArray(arr, size);
+    printf("\n");
+
+    printf("The sorted array is : \n");
+    int* sortedArr = SelectionSort(arr, size);
+    PrintArray(sortedArr, size);
+
+    // Array taken as user input
     unsigned int N = getSize();
     int* A = InputArray(N);
+    printf("The given array is : \n");
     PrintArray(A, N);
+    printf("\n");
 
-    printf("The sorted array is : ");
-    PrintArray(SelectionSort(A, N), N);
+    printf("The sorted array is : \n");
+    int* sortedA = SelectionSort(A, N);
+    PrintArray(sortedA, N);
+
 }
