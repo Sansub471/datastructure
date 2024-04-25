4d786fddc74eb01ba71481c23ac7673ca706782c
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-04-25 22:33:22 +0545

    Optimized bubble sort code written and working.

--- sorting/bubble.c
+++ sorting/bubble.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdbool.h>
 
 #include"utilities.c"
 
@@ -10,9 +11,32 @@ void BubbleSort(int* A, unsigned int N){
                 swap(&A[j], &A[j+1]);
             }
         }
+    //printf("Pass : %d\n", i);
+    //PrintArray(A, N);
     }
 }
 // Time complexity : O(n^2)
+
+void OptimizedBubbleSort(int* A, unsigned int N){
+    int i,j;
+    for(i=0; i< N - 1; i++){
+        bool swapped = false;
+        for(j=0; j< N - i - 1; j++){
+            if(A[j] > A[j+1]){
+                swap(&A[j], &A[j+1]);
+                swapped = true;
+            }
+        }
+        //printf("Pass : %d\n", i);
+        //PrintArray(A, N);
+
+        // if no two elements swapped by inner loop, break
+        if(swapped == false) break;
+    }
+}
+// Best case : O(n)
+// Average case : O(n^2)
+// Worst case : O(n^2)
 
  int main(){
     // Let's try with the given data
@@ -25,6 +49,17 @@ void BubbleSort(int* A, unsigned int N){
     BubbleSort(A, N);
     printf("The sorted array is: \n");
     PrintArray(A, N);
+    printf("\n");
+
+    int B[] = {89, 34, 1, 3, -15, 37, 12, 23, 21, 10};
+    unsigned int size = sizeof(B) / sizeof(B[0]);
+    printf("The given array is : \n");
+    PrintArray(B, size);
+    printf("\n");
+
+    OptimizedBubbleSort(B, size);
+    printf("The sorted array is: \n");
+    PrintArray(B, size);
     printf("\n");
     return 0;
  }
