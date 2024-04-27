ae218da081b85047595c3185a0b6758d9747084e
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-04-27 09:15:50 +0545

    Header file created fro bubble.h

---
+++ sorting/bubble.h
@@ -0,0 +1,5 @@
+#ifndef BUBBLE_SORT
+#define BUBBLE_SORT
+void BubbleSort(int* A, unsigned int N);
+void OptimizedBubbleSort(int* A, unsigned int N);
+#endif
--- sorting/insertion.c
+++ sorting/insertion.c
@@ -1,5 +1,8 @@
 #include<stdio.h>
 #include"utilities.c"
+
+
+
 int main(){
     return 0;
 }
