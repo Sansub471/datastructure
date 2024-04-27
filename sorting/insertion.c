82f5a619b162af2f70257d1689c55a86be667968
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-04-27 11:38:10 +0545

    Insertion sort algorithm completed.

--- sorting/insertion.c
+++ sorting/insertion.c
@@ -3,9 +3,34 @@
 #include"utilities.c"
 
 void InsertionSort(int* A, unsigned int N){
-    
+    int i,j,value,hole;
+    for(i=0; i < N; i++){
+        value = A[i];
+        hole = i;
+        while(hole > 0 && A[hole-1] > value){
+            {
+                A[hole] = A[hole - 1];
+                hole--;
+            }
+        A[hole] = value;
+        }
+    }
 }
 
 int main(){
+    // Let's show one example without user input
+    int arr[] = {56,12,7,21,67,23,17,9,13};
+    unsigned int size = sizeof(arr) / sizeof(arr[0]);
+    
+    printf("The given array is : \n");
+    PrintArray(arr, size);
+    printf("\n");
+
+    InsertionSort(arr, size);
+    printf("The sorted array is : \n");
+    PrintArray(arr, size);
+    printf("\n");
+
+
     return 0;
 }
