4c8f36e7ce0438be848b959e253d6f25a61f4b46
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-05-23 18:12:10 +0545

    The partition index is implemented.

--- sorting/quick.c
+++ sorting/quick.c
@@ -3,6 +3,22 @@
 #include"utilities.c"
 #include"quick.h"
 
+int Partition(int* A, int start, int end){
+    int pivot = A[end]; // pivot be the last element
+    int pIndex = start; // set partition index as start initially
+    int i;
+
+    // Goal : push elements lesser than pivot to the left of pivot
+    for(i=start; i <= end - 1 ; i++){
+        if(A[i] <= pivot){
+            swap(&A[i], &A[pIndex]); // swap if element is lesser than pivot
+            pIndex++;
+        }
+    }
+    swap(&A[pIndex], &A[end]); // swap pivot with element at partition index
+    return pIndex;
+}
+
 int main(){
     
     return 0;
