fff68d9bd761f47f2932b16f74dd212fb7e022e9
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-05 23:23:44 +0545

    Code still looks bad, need to make it look better.

--- Temp_lates/templatesLesson1.cpp
+++ Temp_lates/templatesLesson1.cpp
@@ -1,1 +1,9 @@
 // Brief introduction to templates.
+// The simple idea is to pass the data type as a parameter so that 
+//  we don’t need to write the same code for different data types
+
+#include<iostream>
+
+int main(){
+    return 0;
+}
