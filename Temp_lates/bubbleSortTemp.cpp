4130e33682eb1e7778bc2e716d7c8c8ec2d02128
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-13 00:49:43 +0545

    The bubbleSortTemp is moved back to the Temp_lates folder again.

--- Temp_lates/bubbleSortTemp.cpp
+++ Temp_lates/bubbleSortTemp.cpp
@@ -11,7 +11,6 @@ template <typename T> void PrintArray(T* A, unsigned int N){
         std::cout<<"Can't print NULL array!\n";
         return;
     }
-    std::cout<<"The elements are : ";
     unsigned int i;
     for(i=0; i<N; i++){
         std::cout<<"\t"<< *A;
