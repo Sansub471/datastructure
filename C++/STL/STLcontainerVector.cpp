395ef758a55877595342942cf4e101d3a03695a6
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-17 23:23:13 +0545

    First vector program in C++

--- C++/STL/STLcontainerVector.cpp
+++ C++/STL/STLcontainerVector.cpp
@@ -1,1 +1,14 @@
 // STL container vector 
+#include<iostream>
+#include<vector>
+
+int main(){
+    // create a vector of int type
+    std::vector<int> numbers {1,2,3,4,5,6};
+
+    // print vector element using ranged loop
+    for(int number : numbers){
+        std::cout<< number <<"\t";
+    }
+    return 0;
+}
