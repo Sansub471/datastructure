96bd8490bcc757e4c24efeeb33de9e553adc6a89
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-22 14:14:30 +0545

    Passing vector to functions.

--- C++/STL/STLVector.cpp
+++ C++/STL/STLVector.cpp
@@ -5,6 +5,23 @@
 // The type parameter <T> specifies the type of the vector.
 // The size is not required, it can grow dynamically during execution.
 
+void PrintVector(std::vector<int>& vec){
+    std::cout<<"The elements are : ";
+    for(const int& i : vec){
+        std::cout<<i<<"\t";
+    }
+    std::cout<<std::endl;
+}
+
+
+template<typename T>
+    void PrintingVector(std::vector<T>& vec){
+        std::cout<<"The elements from printing functions : ";
+        for(const int& elem : vec){
+            std::cout<<elem<<"\t";
+        }
+    std::cout<<std::endl;
+    }
 int main(){
     // Initialization Method I
     // 1. Initializer list
@@ -16,6 +33,8 @@ int main(){
     // Initialization Method II
     std::vector<int> vec3(5,12);// 5 is size and 12 is the value
     // equivalent to : std::vector<int> vec3 = {12,12,12,12,12}
+
+    std::vector<double> vec4 {12.34, 56.34, 76.23, 89.23, 99.23, 34.12};
 
     // cosnt reference ranged loop
     std::cout<<"The vector2 elements : ";
@@ -38,5 +57,13 @@ int main(){
     // Less efficient for large complex values.
     // copies the elements of vec3 to 'int i'
 
+
+    // Basic operations
+    // 1. Add elements : insert an element into the end of the vector
+    vec2.push_back(99);
+    vec2.push_back(37);
+
+    PrintVector(vec2);
+    PrintingVector(vec4);
     return 0;
 }
