ad67b00e754648d1dede015cef5fe5d3cad5c7df
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-26 15:07:27 +0545

    The basic and common method of deque.

--- C++/STL/STLDequeue.cpp
+++ C++/STL/STLDequeue.cpp
@@ -2,7 +2,7 @@
 #include<deque>
 
 template<typename T>
-    void PrintDequeue(std::deque<T>& dq){
+    void PrintDeque(std::deque<T>& dq){
         for(const auto& elem: dq){
             std::cout<<elem<<"  ";
         }
@@ -17,13 +17,43 @@ int main(){
     std::deque<std::string> dqwords {"queue", "meow", "few", "sew", "dew", "hue"};
 
     std::cout<<"The numbers are :"; 
-    PrintDequeue(dqnumbers);
+    PrintDeque(dqnumbers);
 
     std::cout<<"The words are   : "; 
-    PrintDequeue(dqwords);
+    PrintDeque(dqwords);
 
     // Other ways to create dequeu
     // 1. Fill Constructor Method
+
+    std::deque<int> deque1(5,12); // (size, val)
+    std::cout<<"The filled deque is : ";
+    PrintDeque(deque1);
+
+    // 2. Range method initialization
+    std::deque<int> dupnumbers(dqnumbers.begin() + 2, dqnumbers.end()-2);
+    // end point is exclusive
+    std::cout<<"The copied deque is : ";
+    PrintDeque(dupnumbers);
+
+    // Common methods of Deque
+    // 1. push_back() : insert element at the back
+    // 2. push_front() : insert element at the front
+    // 3. pop_back() : removes element from the front
+    // 4. pop_front() : removes element from the front
+    // 5. front() : returns the elements at the front
+    // 6. back() : returns the element at the back
+    // 7. at() : sets/ returns the element at specified index
+    // 8. size() : returns the number of elements
+    // 9. empty() : returns true if the deque is empty
+
+    // Can use [] operator to access element at the given index.
+    // Not preferred as it doesn't throw out of bound error.
+
+    // at() method can change element as well
+    deque1.at(0) = 13;
+    std::cout<<"The filled deque is : ";
+    PrintDeque(deque1);
     
+
     return 0;
 }
