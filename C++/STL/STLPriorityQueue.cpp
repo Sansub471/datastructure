79974c7124fdec62ea4ac055302aaae50299f262
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-27 22:44:46 +0545

    Priority queue lesson starts.

---
+++ C++/STL/STLPriorityQueue.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<queue>
+
+int main(){
+
+    // Syntax : std::priority_queue<type> pq;
+
+    std::priority_queue<int> primes;
+    std::priority_queue<std::string> words;
+
+    // by default STL priority queue gives the largest element the highest
+    // priority.
+
+    // Methods : 
+    return 0;
+}
