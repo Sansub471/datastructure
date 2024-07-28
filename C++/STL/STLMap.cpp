3bd2588fe318043d435bf030e267ca3817b3120d
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-07-28 12:02:29 +0545

    Find(key) functin in Map.

--- C++/STL/STLMap.cpp
+++ C++/STL/STLMap.cpp
@@ -5,15 +5,29 @@
 template <typename key, typename value>
     void PrintMap(std::map<key, value>& mapData){
         std::cout<<"The values are : ";
-        for(int i = 1; i<= mapData.size(); ++i){
-            std::cout<<mapData[i]<<" ";
+        for(const auto& pair: mapData){
+            std::cout<< pair.second << " ";
+        }
+        std::cout<<std::endl;
+
+        std::cout<<"\nThe key-value pairs are :\n";
+        typename std::map<key, value>::iterator iter;
+        for(iter = mapData.begin(); iter != mapData.end(); ++iter){
+            std::cout<< iter->first << " ---> " << iter->second << std::endl; // iter-> is same as (*iter).first
+        }
+        std::cout<<std::endl;
+
+        // Another method
+        std::cout<<"\nThe key value pairs are : \n";
+        for(auto itr = mapData.begin(); itr != mapData.end(); ++itr){
+            std::cout<<itr->first << " ---> " << (*itr).second << std::endl;
         }
         std::cout<<std::endl;
 
     }
 int main(){
     // Syntax : std::map<key_type, value_type> map_name = {{key1, val1},{key2, val2}...}
-    std::map<int, std::string> marks = {{1, "Subekshya"}, {3, "Rabin"}, {5, "Babin"}, {2, "Ramesh"}, {6, "Sumesh"},
+    std::map<int, std::string> students = {{1, "Subekshya"}, {3, "Rabin"}, {5, "Babin"}, {2, "Ramesh"}, {6, "Sumesh"},
                                         {4, "Joe"}, {7, "Rachel"}, {9, "Ranjana"}, {8, "Subekshya"}};
     // = operator is optional
     std::map<char, std::string> alphabet {{'A', "Apple"}, {'B', "Ball"}, {'C', "Cat"}, {'D', "Dog"}, {'E', "Elephant"},
@@ -33,19 +47,42 @@ int main(){
     // using the insert() and make_pair() functions
     // map_name.insert(std::make_pair(key, value));
 
-    marks[10] = "Aayusha";
-    marks[10] = "Aayesha"; // Adding duplicate keys
+    students[10] = "Aayusha";
+    students[10] = "Aayesha"; // Adding duplicate keys
     // it overwrites previous value
 
     alphabet.insert(std::make_pair('H', "Horse"));
     alphabet.insert(std::make_pair('H', "Honey")); // duplicate key
 
-    std::cout<<"-------------Student and Marks-----------: \n";
-    PrintMap(marks);
+    std::cout<<"The students in Students map are : ";
+    for(int i = 1; i<= students.size(); ++i){
+            std::cout<<students[i]<<" "; // i is in the keys of map students
+        }
+    std::cout<<std::endl;
 
 
+    std::cout<<"-------------Student & Roll Number-----------: \n";
+    PrintMap(students);
+
+    std::cout<<"-------------Alphabet--------------------:\n";
+    PrintMap(alphabet);
 
 
+    // find() function for maps
+    // map_name.find(key)
+
+    std::map<int, std::string>::iterator iter;
+    int roll = 9;
+    iter = students.find(roll); // find the key 9 in students map
+    // returns an iterator pointing to the element if the element exists
+    // otherwise iterator to the students.end()
+    if(iter != students.end()){
+        std::cout<<"The student, Roll : "<< iter->first << " Name : " << iter->second << std::endl;
+        // you could access iterator by : (*iter).first and (*iter).second as well
+    }
+    else{
+        std::cout<<"Student will roll number " << roll << " not found in records."<<std::endl;
+    }
 
     return 0;
 }
