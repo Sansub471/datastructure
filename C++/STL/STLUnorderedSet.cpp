36ae6f17106db5dc15c8d8ff5fcf0d4e6dff0d7a
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-01 22:21:14 +0545

    All method of initialization.

--- C++/STL/STLUnorderedSet.cpp
+++ C++/STL/STLUnorderedSet.cpp
@@ -18,15 +18,24 @@ int main(){
 
     // Creating unorderedset
     std::unordered_set<int> prime_set = {3,2,7,5,13,11,17,5,5,3,3}; // duplicates will be removed
-    std::unordered_set<std::string> words; // empty set
+    
 
     std::cout<<"Set of prime numbers : ";
     PrintUnorderedSet(prime_set); // check the output
     // We can't use [] operator to add new element
 
-
+    
     std::unordered_set<std::string> usetwords = {"here", "there", "fear", "pear",
                                                     "shear", "gear", "mere"};
+    std::cout<<"The random words are : ";
     PrintUnorderedSet(usetwords);
+
+    // Range initialization method
+    std::unordered_set<std::string>::iterator itr = usetwords.begin();
+    //++itr; ++itr; // index 2
+    std::unordered_set<std::string> words(usetwords.begin(), usetwords.end());
+    std::cout<<"Ranged intialized words : ";
+    PrintUnorderedSet(words);
+
 
 }
