aab51fe93db1f6956e0abc7858525a5262ad8f15
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-02 10:27:17 +0545

    Created and intilialized unordered multiset.

--- C++/STL/STLUnorderedMultiset.cpp
+++ C++/STL/STLUnorderedMultiset.cpp
@@ -1,8 +1,34 @@
 #include<iostream>
 #include<unordered_set>
 
-int main(){
-    return 0;
+template<typename T>
+    void PrintUnMultiset(std::unordered_multiset<T>& unMulSet){
+        // for(const auto& elem : unMulSet){
+        //     std::cout<< elem << "  ";
+        // }
+        // std::cout<<std::endl;
 
-    // today is the day for me.
+        for(auto iter=unMulSet.begin(); iter!= unMulSet.end(); ++iter){
+            std::cout<< *iter << "  ";
+        }
+        std::cout<<std::endl;
+    }
+
+int main(){
+
+    std::unordered_multiset<int> unprime_set = {3,2,7,5,13,11,17,5,5,3,3}; // duplicates allowed
+    // multiset in descending order
+    std::unordered_multiset<int> unnumbers = {2,3,6,7,9,11,4,1,13,27,31,16,3,7,2,3,2,3};
+
+    std::unordered_multiset<std::string> unmulsetwords = {"here", "there", "fear", "pear",
+                                                    "shear", "gear", "mere", "here", "share"};
+    std::cout<<"Multiset of prime numbers : ";
+    PrintUnMultiset(unprime_set);
+
+    std::cout<<"Multiset of numbers : ";
+    PrintUnMultiset(unnumbers);
+
+    std::cout<<"Multiser of words : ";
+    PrintUnMultiset(unmulsetwords);
+    return 0;
 }
