fdb814ebe464d343eb34dc030ee1664e3dbce902
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-02 10:42:43 +0545

    The basic functions lessons on.

--- C++/STL/STLUnorderedMultiset.cpp
+++ C++/STL/STLUnorderedMultiset.cpp
@@ -37,10 +37,41 @@ int main(){
     // 3. clear()  : remove all the elements from a multiset
     // 4. empty()  : check if the multiset is empty
     // 5. size()   : returns the size of the multiset
-    // 6. found()  : find the occurence of a value
+    // 6. find()   : find the occurence of a value
     // 7. count()  : count the frequency of a value
 
     // Insert
+    unnumbers.insert({1,7,4,3,5,1,7,99});
+    unnumbers.insert(98);
+    std::cout<<"Numbers after insertion : ";
+    PrintUnMultiset(unnumbers);
+
+    // Delete valued from an unordered multiser
+    unnumbers.erase(3); // erase all instances of 3
+    unnumbers.erase(2);
+    unnumbers.erase(1000); // no error thrown if not found
+    // clear() remove all
+    std::cout<<"Numbers after deletion : ";
+    PrintUnMultiset(unnumbers);
+
+    // empty() and size() very clear
+    std::cout<<"The size of unprime_set : " << unprime_set.size() << std::endl;
+
+    // find() and count() methods
+    // similar to previous lessons
+    std::string word = "shear";
+    std::unordered_multiset<std::string>::iterator iter = unmulsetwords.find(word);
+    // returns the iterator to the element if found, otherwise the end()
+    if(iter != unmulsetwords.end()){
+        std::cout<< word << " found in the unordered multiset.\n";
+    }
+    else{
+        std::cout<< word << " not found in the unordered multiset.\n";
+    }
+
+    int num = 78;
+    std::cout<<"Number " << num << " occurs " << unnumbers.count(num) << " times.\n";
+    
     
     return 0;
 }
