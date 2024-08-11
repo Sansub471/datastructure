f86d2a5ec89a0af09213e8eac90d850a4afa0ceb
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-11 20:43:51 +0545

    Revision lesson of revusrion concepts.

--- recursion/factorial.cpp
+++ recursion/factorial.cpp
@@ -14,3 +14,4 @@ int main(){
     std::cout<<"The factorial of " << n << " is " << factorial(n) << std::endl;
     return 0;
 }
+// Revision of recursion
--- recursion/fibonacci.cpp
+++ recursion/fibonacci.cpp
@@ -73,3 +73,5 @@ int main(){
 }
 
 // 46th is the largest Fibonacci using integer data type, larger will overflow.
+
+// Revision of fibonacci
