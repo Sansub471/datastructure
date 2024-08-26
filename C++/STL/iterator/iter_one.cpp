732cf089cdc64edeff769e57df10fd809a07a529
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-26 06:32:29 +0545

    Iterator types and supported operations.

--- C++/STL/iterator/iter_one.cpp
+++ C++/STL/iterator/iter_one.cpp
@@ -1,5 +1,20 @@
 #include<iostream>
 
+// operators supported by iterators
+// Iterator Type	Supported Operators
+// Input Iterator	++, *, ->, ==, !=
+// Output Iterator	++, *, =
+// Forward Iterator	++, *, ->, ==, !=
+// Bidirectional Iterator	++, --, *, ->, ==, !=
+// Random Access Iterator	++, --, *, ->, [], +, -, <, <=, >, >=, ==, !=
+
+// Iterator Type	Data type
+// Input Iterator	            eg. input stream
+// Output Iterator	            eg. output stream
+// Forward Iterator	            eg. forward_list
+// Bidirectional Iterator	    eg. list, set, multiset, map, and multimap
+// Random Access Iterator	    eg. vector, deque, array
+
 int main(){
 
     return 0;
