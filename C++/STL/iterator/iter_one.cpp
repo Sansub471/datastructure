c8e228d0aa2b375ab2f3d00209e670ee3d2fc1e3
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-27 07:35:02 +0545

    Starting the day with algorithm class.

--- C++/STL/iterator/iter_one.cpp
+++ C++/STL/iterator/iter_one.cpp
@@ -44,5 +44,27 @@ int main(){
     itervec++;
     std::cout<<"The value of the constant --iterator is : " << *itervec << std::endl;
 
+    std::vector<int>::reverse_iterator riter = numbers.rbegin(); // last element; end() - 1 element
+    // but not same as end() - 1 iterator
+    // reverse iterator and normal iterator are not compatible
+    std::vector<int>::iterator niter = numbers.end() - 1; // normal iterator
+    std::cout<<"The rbegin is : " << *riter  << std::endl;
+    std::cout<<"The end() -1 is : " << *niter << std::endl;
+
+    std::vector<int>::iterator iter1 = numbers.begin();
+    std::vector<int>::reverse_iterator riter2 = numbers.rend(); // theoretical element just before the first element
+    std::advance(iter1, 5);
+    std::advance(iter1, -3); // expect 2
+    std::cout<<"The value of iter1 is : " << *iter1 << std::endl;
+
+    iter1 = std::prev(iter1, 2);
+    iter1 = std::next(iter1, 4); // expect 4
+    std::cout<<"The value of iter1 is : " << *iter1 << std::endl;
+
+    iter1 = std::prev(iter1, -2); // expect 7; actually goes forward: current -(-7)
+    std::cout<<"The value of iter1 is : " << *iter1 << std::endl;
+
+    iter1 = std::next(iter1, -3); // expect 3; actually goes backward: current + (-3)
+    std::cout<<"The value of the iter1 is : " << *iter1 << std::endl;
     return 0;
 }
