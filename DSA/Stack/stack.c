8eb40f6dfe41241329b1f89b0d077c8bca8ff1c4
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-30 20:49:10 +0545

    Stack related problems kept and organized.

---
+++ DSA/Stack/stack.c
@@ -0,0 +1,54 @@
+//Array based implementation of stack.
+#include<stdio.h>
+#define MAX_SIZE 101
+int A[MAX_SIZE];
+int top = -1;
+
+void Push(int x){
+    if(top == MAX_SIZE -1){
+        printf("Error: stack overflow.\n");
+    }
+    A[++top] = x; //pre-increment operator
+    //Increment will happend before assignment.
+}
+
+void Pop(){
+    if (top == -1){
+        printf("Error: No element to pop\n");
+        return ;
+    }
+    top--;
+}
+
+int Top(){
+    if (top == -1){
+        printf("Empty stack.\n");
+    }
+    else{
+        return A[top];
+    }
+}
+
+//To verify the operations are happening correctly
+void Print(){
+    int i;
+    printf("Stack : ");
+    for(i=top; i>-1; i--){
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+}
+int main(){
+    Push(2); Print();
+    printf("Top of stack is : %d\n", Top());
+    Push(5); Print();
+    Push(7); Print();
+    Push(8); Print();
+    Push(10); Print();
+    printf("Top of stack is : %d\n", Top());
+
+    printf("Popping operation.\n");
+    Pop(); Print();
+    printf("Top of stack is : %d\n", Top());
+    return 0;
+}
