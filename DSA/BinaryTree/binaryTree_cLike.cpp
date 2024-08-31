956bcba980953df97b8e48ad128bc5f2d9f49b44
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-08-31 17:33:41 +0545

    IsFullBinary function works.

--- DSA/BinaryTree/binaryTree_cLike.cpp
+++ DSA/BinaryTree/binaryTree_cLike.cpp
@@ -1,6 +1,7 @@
 // The structure implementation is more C like
 #include"binaryTree.hpp"
 #include"isFullBinaryTree.cpp"
+#include"binaryTreeTraversal.cpp"
 
 int main(){
     struct Node* root = newNode(7);
@@ -21,6 +22,8 @@ int main(){
     std::cout<<"The tree is Full Binary or not? : " << isFullBinaryTree(root) << std::endl;
     // to check if full binary add one more element at the extreme left and check if full binary
     root->left->left->left = newNode(17);
+    std::cout<<"The tree is Full Binary or not? : " << isFullBinaryTree(root) << std::endl;
+    root->left->left->right = newNode(23);
     std::cout<<"The tree is Full Binary or not? : " << isFullBinaryTree(root) << std::endl;
     return 0;
 }
