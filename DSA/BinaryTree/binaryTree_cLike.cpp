83c5be531c7eac264240794813a152998bd57cdb
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-09-01 16:38:39 +0545

    Adding code for perfect binary and complete binary tree.

--- DSA/BinaryTree/binaryTree_cLike.cpp
+++ DSA/BinaryTree/binaryTree_cLike.cpp
@@ -1,7 +1,10 @@
 // The structure implementation is more C like
 #include"binaryTree.hpp"
-#include"isFullBinaryTree.cpp"
 #include"binaryTreeTraversal.cpp"
+
+#include"isFullBinary.cpp"
+#include"isPerfectBinary.cpp"
+#include"isCompleteBinary.cpp"
 
 int main(){
     struct Node* root = newNode(7);
@@ -25,5 +28,29 @@ int main(){
     std::cout<<"The tree is Full Binary or not? : " << isFullBinaryTree(root) << std::endl;
     root->left->left->right = newNode(23);
     std::cout<<"The tree is Full Binary or not? : " << isFullBinaryTree(root) << std::endl;
+
+    std::cout<<"The Depth of the tree is : " << depth(root) << std::endl;
+
+    struct Node* pbt = perfectBinaryTree();
+    std::cout<<"\n-------------Perfect Binary Tree--------"<<std::endl;
+    std::cout<<"The inorder traversal of tree : "; Inorder(pbt); std::cout<<std::endl;
+    int d = depth(pbt);
+    std::cout<<"The depth of the tree : " << d << std::endl;
+
+    std::cout<<"Is the tree a PBT ? " << isPerfectBinaryTree(pbt, d) << std::endl;
+
+    struct Node* cbt = completeBinaryTree();
+    std::cout<<"\n-------------Complete Binary Tree--------"<<std::endl;
+    std::cout<<"The inorder traversal of tree : "; Inorder(cbt); std::cout<<std::endl;
+    int countNodes = countNodesNumber(cbt);
+    std::cout<<"The number of nodes is : " << countNodes << std::endl;
+    int index = 0; 
+    if(isCompleteBinary(cbt, index, countNodes)){
+        std::cout<<"The tree is a complete binary tree."<<std::endl;
+    }
+    else{
+        std::cout<<"The tree is not a complete binary tree."<<std::endl;
+    }
+
     return 0;
 }
