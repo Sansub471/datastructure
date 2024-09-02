5a04980ad5229068a9ddb343ba497ea836ccf0b1
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-09-02 12:46:37 +0545

    To check if the tree is balanced binary tree or not?

---
+++ DSA/BinaryTree/isBalancedBinary.cpp
@@ -0,0 +1,35 @@
+#include"binaryTree.hpp"
+
+
+struct Node* balancedBinaryTree(){
+    // give a tree to test balancec binary condition
+    struct Node* root = newNode(1);
+    root->left = newNode(2); 
+    root->right = newNode(3);
+
+    root->left->left = newNode(4);
+    root->left->right = newNode(5);
+    //root->left->right->left = newNode(6);
+    return root;
+}
+
+bool isBalancedBinary(struct Node* root, int* height){
+    int leftheight = 0, rightHeight = 0;
+    bool l=false, r=false;
+
+    if(root==nullptr){
+        *height = 0;
+        return true;
+    }
+
+    l = isBalancedBinary(root->left, &leftheight);
+    r = isBalancedBinary(root->right, &rightHeight);
+
+    *height = (leftheight > rightHeight ? leftheight : rightHeight) + 1;
+    if(std::abs(leftheight - rightHeight) >= 2){
+        return false;
+    }
+    else{
+        return l && r;
+    }
+}
