2ac33e1a25829765fc6fe53e5e63bfd71e4cc6be
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-09-03 17:48:44 +0545

    Insert function completed.

--- DSA/BinaryTree/binarySearchTree.cpp
+++ DSA/BinaryTree/binarySearchTree.cpp
@@ -15,6 +15,19 @@ template <typename T>
     }
 
 
+template <typename T>
+    T* InsertBST(T* root, int data){
+        if(root == nullptr) return new T(data); // create a new node
+
+        if(data < root->data){
+            root->left = InsertBST(root->left, data);
+        }
+        else if(data > root->data){
+            root->right = InsertBST(root->right, data);
+        }
+        return root;
+    }
+
 int main(){
     SNode* bst1 = BST_ONE();
     CNode* bst2 = BST_TWO();
@@ -26,5 +39,16 @@ int main(){
     int key = 5;
     std::cout<<"Tree one has " << key << " ? : " << SearchBST(bst1, key) << std::endl;
     std::cout<<"Tree two has " << key << " ? : " << SearchBST(bst2, key) << std::endl;
+
+    bst1 = InsertBST(bst1, key);
+    bst2 = InsertBST(bst2, key);
+    std::cout<<std::endl;
+    std::cout<<"Tree one(inorder) : "; inorderDFS(bst1); std::cout<<std::endl;
+    std::cout<<"Tree two(inorder) : "; inorderDFS(bst2); std::cout<<std::endl;
+    std::cout<<std::endl;
+
+    std::cout<<"Tree one has " << key << " ? : " << SearchBST(bst1, key) << std::endl;
+    std::cout<<"Tree two has " << key << " ? : " << SearchBST(bst2, key) << std::endl;
+
     return 0;
 }
