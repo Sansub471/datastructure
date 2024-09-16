f79e1d3c6b8334df38ff52c58cfce5acae1cc5d9
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-09-16 15:07:16 +0545

    Helper functions and node defined for red black tree.

--- DSA/RedBlackTree/redBlackTree.cpp
+++ DSA/RedBlackTree/redBlackTree.cpp
@@ -1,4 +1,25 @@
+#include"redBlackTree.hpp"
 
+void RedBlackTree::initializeNULLNode(Node* node, Node* parent){
+    node->data = 0;
+    node->parent = parent;
+    node->left = nullptr;
+    node->right = nullptr;
+    node->color = false;
+}
+
+void RedBlackTree::preOrderHelper(Node* node){
+
+}
+
+
+void RedBlackTree::inOrderHelper(Node* node){
+
+}
+
+void RedBlackTree::postOrderHelper(Node* node){
+
+}
 int main(){
     return 0;
 }
--- DSA/RedBlackTree/redBlackTree.hpp
+++ DSA/RedBlackTree/redBlackTree.hpp
@@ -3,7 +3,29 @@
 
 #ifndef RED_BLACK_NODE
 #define RED_BLACK_NODE
+// RED if true, BLACK is false
+    class Node {
+    public:
+        int data;
+        Node* parent;
+        Node* left;
+        Node* right;
+        bool color;
+    };
 
+    class RedBlackTree{
+        private:
+            Node* root;
+            Node* TNULL;
+        
+            void initializeNULLNode(Node* node, Node* parent);
+            // preorder
+            void preOrderHelper(Node* node);
+            // inorder
+            void inOrderHelper(Node* node);
+            // post order
+            void postOrderHelper(Node* node);
+    };
 #endif
 
 
