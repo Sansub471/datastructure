188671a29278110ca4a8200733e74a297eec0dd3
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-09-16 18:17:13 +0545

    Class definition going on.

--- DSA/RedBlackTree/redBlackTree.hpp
+++ DSA/RedBlackTree/redBlackTree.hpp
@@ -3,7 +3,7 @@
 
 #ifndef RED_BLACK_NODE
 #define RED_BLACK_NODE
-// RED if true, BLACK is false
+// RED is true, BLACK is false
     class Node {
     public:
         int data;
@@ -12,7 +12,8 @@
         Node* right;
         bool color;
     };
-
+#endif
+    typedef Node* NodePtr;
     class RedBlackTree{
         private:
             Node* root;
@@ -25,9 +26,38 @@
             void inOrderHelper(Node* node);
             // post order
             void postOrderHelper(Node* node);
+
+            // Search helper function
+            Node* searchTreeHelper(Node* node, int key);
+        
+        public:
+            RedBlackTree(); // constructor
+            void preOrder(){
+                preOrderHelper(this->root);
+                }
+
+            void inOrder(){
+                inOrderHelper(this->root);
+                }
+
+            void postOrder(){
+                postOrderHelper(this->root);
+                }
+
+            Node* searchTree(int key){
+                return searchTreeHelper(this->root, key);
+            }
+
+            Node* minimun(Node* node);
+            Node* maximum(Node* node);
+            Node* successor(Node* node);
+            Node* predecessor(Node* node);
+
+            void leftRotate(Node* X);
+            void rightRotate(Node* Y);
+            
+
+            
+
     };
-#endif
-
-
-
 #endif
