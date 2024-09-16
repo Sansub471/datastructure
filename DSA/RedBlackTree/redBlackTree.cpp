95067ef382096f6023fa30ee6c6636d8910a1398
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-09-16 18:16:54 +0545

    maximum(), minimum(), successor(), predecessor(), leftRotate(), rightRotate()

--- DSA/RedBlackTree/redBlackTree.cpp
+++ DSA/RedBlackTree/redBlackTree.cpp
@@ -48,6 +48,74 @@ RedBlackTree::RedBlackTree(){
     root = TNULL;
 }
 
+Node* RedBlackTree::minimun(Node* root){
+    while(root->left != TNULL){
+        root = root->left;
+    }
+    return root;
+}
+
+Node* RedBlackTree::maximum(Node* root){
+    while(root->right != TNULL){
+        root = root->right;
+    }
+    return root; 
+}
+
+Node* RedBlackTree::successor(Node* root){
+    if(root->right != TNULL) return minimun(root->right);
+    Node* node = root->parent;
+    while(node != TNULL && root == node->right){
+        root = node;
+        node = node->parent;
+    }
+    return node;
+}
+
+Node* RedBlackTree::predecessor(Node* root){
+    if(root->left != TNULL) return maximum(root->left);
+    Node* node = root->parent;
+    while(node != TNULL && root == node->left){
+        root = node;
+        node = node->parent;
+    }
+    return node;
+}
+
+void RedBlackTree::leftRotate(Node* X){
+    Node* Y = X->right;
+    X->right = Y->left;
+    if(Y->left != TNULL) Y->left->parent = X;
+    Y->parent = X->parent;
+    if(X->parent == nullptr){
+        this->root = Y;
+    }else if(X == X->parent->left){
+        X->parent->left = Y;
+    }else{
+        X->parent->right = Y;
+    }
+    Y->left = X;
+    X->parent = Y;
+}
+
+void RedBlackTree::rightRotate(Node* X){
+    Node* Y = X->left;
+    X->left = Y->right;
+    if(Y->right != TNULL) Y->right->parent = X;
+    Y->parent = X->parent;
+    if(X->parent == nullptr){
+        this->root = Y;
+    }else if(X == X->parent->right){
+        X->parent->right = Y;
+    }else{
+        X->parent->left = Y;
+    }
+    Y->right = X;
+    X->parent = Y;
+}
+
+
+
 int main(){
     return 0;
 }
