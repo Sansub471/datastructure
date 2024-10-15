f09bd32d117b919747c1aa3b4b1e10711e73a7d4
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-15 19:23:02 +0545

    Level order traversal, BFS technique, left-right and right-left works.

--- DSA/BinaryTree/binarySearchTreeMain.cpp
+++ DSA/BinaryTree/binarySearchTreeMain.cpp
@@ -10,10 +10,10 @@ int main(){
     CNode* bst2 = BST_TWO();
 
     std::cout<<"----------------------Binary Search Tree------------------"<<std::endl;
-    std::cout<<"Tree one(inorder) : "; inorderDFS(bst1); std::cout<<std::endl;
-    printTree(bst1);
-    std::cout<<"Tree two(inorder) : "; inorderDFS(bst2); std::cout<<std::endl;
-    printTree(bst2);
+    std::cout<<"Tree one(inorder) : "; Traversal::inorderDFS(bst1); std::cout<<std::endl;
+    Traversal::printTree(bst1);
+    std::cout<<"Tree two(inorder) : "; Traversal::inorderDFS(bst2); std::cout<<std::endl;
+    Traversal::printTree(bst2);
 
     int key = 5;
     std::cout<<"Tree one has " << key << " ? : " << SearchBST(bst1, key) << std::endl;
@@ -22,8 +22,8 @@ int main(){
     bst1 = InsertBST(bst1, key);
     bst2 = InsertBST(bst2, key);
     std::cout<<std::endl;
-    std::cout<<"Tree one(inorder) : "; inorderDFS(bst1); std::cout<<std::endl;
-    std::cout<<"Tree two(inorder) : "; inorderDFS(bst2); std::cout<<std::endl;
+    std::cout<<"Tree one(inorder) : "; Traversal::inorderDFS(bst1); std::cout<<std::endl;
+    std::cout<<"Tree two(inorder) : "; Traversal::inorderDFS(bst2); std::cout<<std::endl;
     std::cout<<std::endl;
 
     std::cout<<"Tree one has " << key << " ? : " << SearchBST(bst1, key) << std::endl;
@@ -33,18 +33,29 @@ int main(){
     key = 7 ; // leaf node deletion
     bst2 = deleteBSTNode(bst2, key);
     std::cout<<"Tree two, deleted node : " << key << std::endl;
-    std::cout<<"Tree two(inorder) : "; inorderDFS(bst2); std::cout<<std::endl;
+    std::cout<<"Tree two(inorder) : "; Traversal::inorderDFS(bst2); std::cout<<std::endl;
 
     // creating a BST with randomly generated array
-    RandomIntElements tree = RandomIntElements(2, 100, 20);
+    RandomIntElements tree = RandomIntElements(2, 100, 10);
     std::vector<int> randomElems = tree.getRandomNumbers();
     std::cout<<"\nThe array to create BST is: "; 
     tree.printElements();
 
     CNode* cbst = createBSTree(static_cast<CNode*>(nullptr), randomElems);
-    printTree(cbst);
+    Traversal::printTree(cbst);
     // nullptr initializes the root pointer to an empty state (i.e., no tree yet).
     // static_cast<BST::CNode*>(nullptr) explicitly casts the null pointer to the desired 
     // type (BST::CNode*) so that the templated function knows the correct node type (CNode*, SNode*, or Node<T>*) to work with.
+
+    std::cout<<"\nThe level order traversal(left-right) is:";
+    Traversal::levelOrder(cbst);
+    std::cout<<std::endl;
+
+    std::cout<<"\nThe level order traversal(right-left) is:";
+    Traversal::levelOrderR(cbst);
+    std::cout<<std::endl;
+
+
+    delete bst2;
     return 0;
 }
