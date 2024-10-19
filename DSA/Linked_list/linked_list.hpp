4f38afcbfd1761f5d9305d4e45868ba7384f1cbc
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-19 12:07:04 +0545

    This is .hpp file of previous .h file of same name.

---
+++ DSA/Linked_list/linked_list.hpp
@@ -0,0 +1,36 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#ifndef LINKED_NODE
+#define LINKED_NODE
+struct Node
+{
+    int data;
+    struct Node* next;
+};
+
+typedef struct Node LinkedNode;
+#endif
+// Insert at the beginning
+struct Node* Insert(struct Node* head, int x);
+
+// Insert at nth position
+struct Node* InsertPosition(struct Node* head, int data, int n);
+
+//Insert at the end.
+struct Node* InsertAtEnd(struct Node* head, int data);
+
+//Delete at nth position
+struct Node* DeletePosition(struct Node* head, int n);
+
+//Reverse by iteration
+struct Node* ReverseByIteration(struct Node* head);
+
+//Reverse by recursion
+void ReverseByRecursion(struct Node* current);
+
+// Print functions
+void PrintList(struct Node*);
+void PrintRecursion(struct Node* head);
+void ReversePrintRecursion(struct Node* head);
+#endif
