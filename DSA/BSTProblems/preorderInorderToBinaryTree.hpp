1c4a71effd11504b472ff5bcbb3a0ee2100f768d
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-19 23:06:04 +0545

    Convert preorder and inorder to a binary tree.

---
+++ DSA/BSTProblems/preorderInorderToBinaryTree.hpp
@@ -0,0 +1,37 @@
+// in place of #indef #def #endif block
+#pragma once
+#include<vector>
+#include"TreeNode.hpp"
+
+class preorderInorderToBinaryTree{
+public:
+    std::vector<int> preorder;
+    std::vector<int> inorder;
+    int preOrderIndex;
+    
+    // the element is guaranteed to be found
+    int search(int start, int end, int value){
+        for(int i = start; i <= end; ++i){
+            if(this->inorder.at(i) == value) return i;
+        }
+        return -1; // for compiler issue
+    }
+    TreeNode* makeTree(int start, int end){
+        if(start > end) return nullptr;
+        int key = this->preorder.at(this->preOrderIndex);
+        int index = this->search(start, end, key);
+        TreeNode* node = new TreeNode(key);
+        this->preOrderIndex++;
+        node->left = makeTree(start, index - 1);
+        node->right = makeTree(index + 1, end);
+        return node;
+        
+    }
+    TreeNode* buildTree(std::vector<int>& preorder, std::vector<int>& inorder) {
+        if(preorder.empty() || inorder.empty()) return nullptr;
+        this->preorder = preorder;
+        this->inorder = inorder;
+        this->preOrderIndex = 0;
+        return makeTree(0, inorder.size() - 1);        
+    }
+};
