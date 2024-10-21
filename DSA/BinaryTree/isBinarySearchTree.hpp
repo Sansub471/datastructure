4858f0b47e308cfcbe7cd71ef08d8f7d2c81a6df
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-21 16:31:03 +0545

    check if given binary tree is BST or not.

--- DSA/BinaryTree/isBinarySearchTree.hpp
+++ DSA/BinaryTree/isBinarySearchTree.hpp
@@ -1,1 +1,19 @@
+#pragma once
+#include"binaryTree.hpp"
+#include<climits>
 
+bool BSThelper(Node* root, long minValue, long maxValue){
+    if(root == nullptr) return true;
+    if(root->data > minValue && root->data < maxValue
+        && BSThelper(root->left, minValue, root->data)
+        && BSThelper(root->right, root->data, maxValue)){
+            return true;
+    }else{
+        return false;
+    }
+}
+
+bool isValidBST(Node* root) {
+    if(root == nullptr) return true;
+    return BSThelper(root, LONG_MIN, LONG_MAX);
+}
