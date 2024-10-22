5893d36651c1ea85360284e623039df7b51a7c65
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-22 22:00:41 +0545

    Check if linked list has a cycle.

---
+++ DSA/LinkedListLeet/linkedListCycle.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+#include<vector>
+#include"../Linked_list/linked_list.hpp"
+#include<unordered_map>
+
+typedef LinkedList::Node ListNode;
+
+bool hasCycleMine(ListNode *head) {
+    if(head == nullptr) return false;
+    std::unordered_map<ListNode*, int> ListMap;
+    int MAX = 10000;
+    int count = 0;
+    ListNode* headptr = head;
+    while(headptr != nullptr){
+        if(!ListMap.empty()){
+            if(ListMap.find(headptr) != ListMap.end()){
+                int pos = ListMap[headptr];
+                return true;
+            }
+        }
+        ListMap[headptr] = count++;
+        headptr = headptr->next;
+
+        if(count > MAX) return false;
+    }
+    return false;
+}
+
+// leet code solution
+bool hasCycle(ListNode *head) {
+    ListNode *fast = head;
+    ListNode *slow = head;   
+    while(fast != nullptr && fast->next != nullptr) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast)
+        return true;
+    }  
+    return false; 
+}
