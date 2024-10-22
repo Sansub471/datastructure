6a9466f90f02843e7f1d4123eafd3c5d2743e2d2
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-22 16:19:36 +0545

    The generic getLinkedlist() is now moved to getLinkList.hpp

--- DSA/LinkedListLeet/addTwoNumbers.hpp
+++ DSA/LinkedListLeet/addTwoNumbers.hpp
@@ -1,17 +1,7 @@
 #pragma once
-
-#include<vector>
 #include"../Linked_list/linked_list.hpp"
 
 typedef LinkedList::Node ListNode;
-
-ListNode* getLinkedList(const std::vector<int>& elems){
-    ListNode* head = nullptr;
-    for(const auto& elem : elems){
-        head = LinkedList::InsertAtEnd(head, elem);
-    }
-    return head;
-}
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
     if(l1 == nullptr && l2 == nullptr) return nullptr;
