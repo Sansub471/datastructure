c1f47245043e9167879332141f85d16005219789
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-24 01:45:38 +0545

    Check if a list is palindrome.

---
+++ DSA/LinkedListLeet/palindromeList.hpp
@@ -0,0 +1,32 @@
+// Chech if a linked list is palindrome
+#pragma once
+#include"../Linked_list/linked_list.hpp"
+
+typedef LinkedList::Node ListNode;
+
+ListNode* reverseList(ListNode* head){
+    ListNode* prevNode = nullptr, *currentNode = head, *nextNode = head;
+    while(currentNode){
+        nextNode = currentNode->next;
+        currentNode->next = prevNode;
+        prevNode = currentNode;
+        currentNode = nextNode;
+    }
+    return prevNode;
+}
+
+bool isPalindrome(ListNode* head) {
+    ListNode* slow = head;
+    ListNode* fast = head;
+    while(fast->next!=NULL && fast->next->next!=NULL){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    ListNode* newHead = reverseList(slow->next);
+    while(newHead){
+        if(head->val != newHead->val)    return false;
+        head = head->next;
+        newHead = newHead->next;
+    }
+    return true;
+}
