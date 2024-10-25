cc34125ae2b6361de1dcd7ee0b4cdbd020ca943b
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-25 16:00:10 +0545

    The median of two sorted arrays

--- DSA/LeetProblems/medianOfTwoSortedArray.hpp
+++ DSA/LeetProblems/medianOfTwoSortedArray.hpp
@@ -1,28 +1,27 @@
 #pragma once
 #include<vector>
-double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+std::vector<int> mergedArrays(std::vector<int>& nums1, std::vector<int>& nums2){
     int p1=0, p2=0;
-    std::vector<int> nums;
+    std::vector<int> merged;
     while(p1 < nums1.size() && p2 < nums2.size()){
-        if(nums1.at(p1) <= nums2.at(p2)){
-            nums.push_back(nums1.at(p1));
-            p1++;
+        if(nums1[p1] <= nums2[p2]){
+            merged.push_back(nums1[p1++]);
         }
         else{
-            nums.push_back(nums2.at(p2));
-            p2++;
+            merged.push_back(nums2[p2++]);
         }
     }
-    while(p1 < nums1.size()){
-        nums.push_back(nums1.at(p1));
-        p1++;
-    }
-    while(p2 < nums2.size()){
-        nums.push_back(nums2.at(p2));
-        p2++;
-    }
-    double pos = (nums.size()+1) / 2.0;
-    int index = (nums.size()+1) / 2;
-    double median = nums.at(index-1) + (pos - index)*(nums.at(index) - nums.at(index-1));
-    return median;  
+    while(p1 < nums1.size()) merged.push_back(nums1[p1++]);
+    while(p2 < nums2.size()) merged.push_back(nums2[p2++]);
+    return merged;
+}
+
+double findMedianSortedArrays(std::vector<int>& nums1, std::vector<int>& nums2) {
+    std::vector<int> merged = mergedArrays(nums1, nums2);
+    int n = merged.size();
+    if(n % 2 == 1){
+        return merged[n / 2];
+    }else{
+        return ((merged[n / 2 - 1] + merged[n / 2]) / 2.0);
+    }      
 }
