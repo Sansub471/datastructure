96d3cc11de809d8eb23b8b4060529eccf0c4a27b
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-27 22:23:59 +0545

    Linked list related problem driving code.

--- DSA/LinkedListLeet/linkedListMain.cpp
+++ DSA/LinkedListLeet/linkedListMain.cpp
@@ -1,11 +1,19 @@
 // All linked list related problems will be run from here.
+#include<vector>
 
+#include"getLinkList.hpp"
 #include"addTwoNumbers.hpp"
+#include"palindromeList.hpp"
 #include"../LeetProblems/sumWithoutOperator.hpp"
 
+#include"mergeTwoLists.hpp"
+#include"reorderList.hpp"
+#include"deleteMiddleNode.hpp"
+
 int main(){
-    std::vector<int> l1 = {9,9,9,9,9,9,9};
-    std::vector<int> l2 = {9,9,9,9};
+    // change the values to meet your requirement
+    std::vector<int> l1 = {1,2,3,4,3,2,1};
+    std::vector<int> l2 = {5};
 
     ListNode* linked1 = getLinkedList(l1);
     ListNode* linked2 = getLinkedList(l2);
@@ -15,12 +23,37 @@ int main(){
     std::cout<<"The second linked list is : ";
     LinkedList::PrintList(linked2);
 
+    // add two numbers
     ListNode* result = addTwoNumbers(linked1, linked2);
     std::cout<<"The sum of linked lists is : ";
     LinkedList::PrintList(result);
 
-    std::cout<<"The sum  = "<< getSum(5, 7) << std::endl;
-    std::cout<<"The difference = " << subtract(12, 5) << std::endl;
+    // mergeTwosortedLists, ascending
+    result = mergeTwoLists(linked1, linked2);
+    std::cout<<"The merged list is : ";
+    LinkedList::PrintList(result);
+
+    // palindrome list
+    std::vector<int> palList {1,2,3,4,3,2,1};
+    ListNode* palLinked = getLinkedList(palList);
+    std::cout<<"Palindrome linked list : ";
+    LinkedList::PrintList(palLinked);
+    bool palindrome = isPalindrome(palLinked);
+    std::cout<<"Palindrome ? " << palindrome <<std::endl;
+
+    // reorder linked list
+    std::vector<int> reorder = {1,2,3,4};
+    ListNode* reorderlist = getLinkedList(reorder);
+    reorderList(reorderlist);
+
+    // delete middle
+    std::vector<int> delMid = {2,1};
+    ListNode* delMidhead = getLinkedList(delMid);
+    std::cout<<"Before deleting middle element : "; 
+    LinkedList::PrintList(delMidhead);
+    delMidhead = deleteMiddle(delMidhead);
+    std::cout<<"After deleting middle element: ";
+    LinkedList::PrintList(delMidhead);
 
     return 0;
 }
