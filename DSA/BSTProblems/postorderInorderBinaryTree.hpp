e676ad4cab79f4eb01940993601148ca2c99038b
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-10-31 19:36:57 +0545

    post and inorder to binary tree code testing phase.

---
+++ DSA/BSTProblems/postorderInorderBinaryTree.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include<vector>
+#include"TreeNode.hpp"
+
+class postorderInorderToBinaryTree {
+public:
+    std::vector<int> inorder;
+    std::vector<int> postorder;
+    int postIndex;
+
+    int search(int start, int end, int value){
+        for(int i = start; i <= end; ++i){
+            if(inorder[i] == value) return i; // sure found
+        }
+        return -1; // for compiler
+    }
+    TreeNode* makeTree(int start, int end){
+        if(start > end ) return nullptr;
+        int key = this->postorder.at(this->postIndex); 
+        int index = search(start, end, key);
+        this->postIndex--;
+        TreeNode* node = new TreeNode(key);
+        node->left = makeTree(start, index - 1);
+        node->right = makeTree(index + 1, end);
+        return node;
+
+    }
+    TreeNode* buildTree(std::vector<int>& inorder, std::vector<int>& postorder) {
+        if(inorder.empty() || postorder.empty()) return nullptr;
+        this->inorder = inorder;
+        this->postorder = postorder;
+        this->postIndex = postorder.size() - 1;
+        return makeTree(0, this->postIndex);
+    }
+};
