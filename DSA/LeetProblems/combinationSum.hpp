b762add45817d9854209219be2d8740a18bf1f68
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-06 23:10:51 +0545

    The combinations sum problem worked

--- DSA/LeetProblems/combinationSum.hpp
+++ DSA/LeetProblems/combinationSum.hpp
@@ -2,26 +2,41 @@
 #include<vector>
 #include<algorithm>
 
-void backtrackCombinations(std::vector<int>& candidates, std::vector<std::vector<int>>& combinations, std::vector<int>& currComb, int target, int index){
-    if(index >= candidates.size()) return;
-    int targetComp = target - candidates[index];
-    if(targetComp > 0) currComb.push_back(candidates[index]);
-    if(targetComp < 0) return;
-    if(targetComp == 0 ){
+void backtrackCombinations(std::vector<int>& candidates, std::vector<std::vector<int>>& combinations, 
+                            std::vector<int>& currComb, int remaining, int index){
+    if(remaining == 0){
+        // valid combination found
         combinations.push_back(currComb);
+        return;
     }
-    backtrackCombinations(candidates, combinations, currComb, targetComp, index);      
+
+    if(remaining < 0){
+        // combination invalid
+        return;
+    }
+
+    for(int i = index; i < candidates.size(); ++i){
+        // choose current candidate
+        int candidate = candidates[i];
+        currComb.push_back(candidate);
+
+        // recur with updated remaining, and i since we can reuse the candidate
+        backtrackCombinations(candidates, combinations, currComb, remaining - candidate, i);
+
+        // backtrack by removing the last added candidate
+        currComb.pop_back();
+    }      
 }
 
 std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int target) {
     std::vector<std::vector<int>> combinations;
     // 1. sort the candidates to prevent duplicate combination
-    std::sort(candidates.begin(), candidates.end());
+    //sort(candidates.begin(), candidates.end()); // not necessary as distinct elements are guaranteed.
 
     // 2. Backtracking and DF search technique
-    for(int i = 0; i < candidates.size(); ++i){
-        std::vector<int> currComb;
-        backtrackCombinations(candidates, combinations, currComb, target, i);
-    }
+    std::vector<int> currComb;
+    backtrackCombinations(candidates, combinations, currComb, target, 0);
     return combinations;
 }
+
+// Time complexity : O(2^N)
