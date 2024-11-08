92aba24e29ce53bd7e06cf9ce5cc19555cf5be00
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-08 15:13:21 +0545

    Combination sum II problem solved for now.

--- DSA/LeetProblems/combinationSum.hpp
+++ DSA/LeetProblems/combinationSum.hpp
@@ -40,6 +40,38 @@ std::vector<std::vector<int>> combinationSum(std::vector<int>& candidates, int t
 }
 
 // Time complexity : O(2^N)
-// Time coplexity for the code added, can you derive yourself?
 
-// understood how the code works now.
+// Combination Sum II
+void backtrack(std::vector<int>& candidates, std::vector<std::vector<int>>& combinations, 
+                std::vector<int>& currComb, int remaining, int startIndex){
+    if(remaining == 0){
+        combinations.push_back(currComb);
+        return;
+    }
+
+    if(remaining < 0){
+        return;
+    }
+
+    for(int i = startIndex; i < candidates.size(); ++i){
+        int candidate = candidates[i];
+        if(i > startIndex && candidates[i - 1] == candidate) continue;
+        int remTarget = remaining - candidate;
+        //if(remTarget < 0) break; // can replace remaining < 0 check
+
+        currComb.push_back(candidate);
+        backtrack(candidates, combinations, currComb, remTarget, i + 1);
+        currComb.pop_back();
+    }
+}
+
+std::vector<std::vector<int>> combinationSum2(std::vector<int>& candidates, int target) {
+    std::vector<std::vector<int>> combinations;
+    std::vector<int> currComb;
+
+    // sort to remove duplicates, candidates are not unique
+    sort(candidates.begin(), candidates.end());
+    int startIndex = 0;
+    backtrack(candidates, combinations, currComb, target, startIndex);
+    return combinations;
+}
