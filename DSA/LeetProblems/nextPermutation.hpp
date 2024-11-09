3a1ea91b3924bc63feee2b8d385f81a3054f3c74
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-09 13:41:17 +0545

    std::next_permutation like behaviour.

--- DSA/LeetProblems/nextPermutation.hpp
+++ DSA/LeetProblems/nextPermutation.hpp
@@ -39,3 +39,32 @@ void nextPermutation(std::vector<int>& nums) {
     // 4. Reverse the suffix, elements after i
     std::reverse(nums.begin() + i + 1, nums.end());
 }
+
+// To make it like std::next_permutation
+// return type bool and return false if i < 0 and return true at the end
+bool next_Permutation(std::vector<int>& nums) {
+    int n = nums.size();
+    int i = n - 2; // 1. i). right most decreasing pair
+    while(i >= 0 && nums[i] >= nums[i + 1]){
+        i--;
+    }
+
+    // ii). no such pair found
+    if( i < 0){
+        std::reverse(nums.begin(), nums.end());
+        return false;
+    }
+
+    //2. Find smallest element larger than nums[i] in the right
+    int j = n - 1;
+    while(nums[j] <= nums[i]){
+        j--;
+    }
+
+    //3. Swap
+    std::swap(nums[i], nums[j]);
+
+    // 4. Reverse the suffix, elements after i
+    std::reverse(nums.begin() + i + 1, nums.end());
+    return true;
+}
