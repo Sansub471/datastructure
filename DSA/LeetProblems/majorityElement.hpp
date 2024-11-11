85dd54460c736dcc61639c4b8e5199ee914c74ba
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-11 20:47:31 +0545

    Majority element in O(nlogn) time .

---
+++ DSA/LeetProblems/majorityElement.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include<vector>
+#include<algorithm>
+int majorityElement(std::vector<int>& nums) {
+    int n = nums.size();
+    std::sort(nums.begin(), nums.end());
+    int count = 0, i;
+    for(i = 0; i < n; ++i){
+        if(i > 0 && nums[i] == nums[i - 1]){
+            ++count;
+            continue;
+        }else{
+            if(count > n/2) return nums[i-1];
+            count = 1;
+        }
+    }
+    if(count > n/2) return nums[i-1];
+    // for compiler
+    return -1;
+}
+
+// Sorting takes O(NlogN) where as after sorting only O(N) hence sorting time is the time complexity
