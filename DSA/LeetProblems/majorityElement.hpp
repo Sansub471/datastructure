25b0b669a880c0eb2ff3c998d6d008afc9ce9543
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-11 23:51:05 +0545

    Find majority element whose count > n/3.

--- DSA/LeetProblems/majorityElement.hpp
+++ DSA/LeetProblems/majorityElement.hpp
@@ -20,3 +20,21 @@ int majorityElement(std::vector<int>& nums) {
 }
 
 // Sorting takes O(NlogN) where as after sorting only O(N) hence sorting time is the time complexity
+
+std::vector<int> majorityElement2(std::vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    std::vector<int> result;
+    int count = 0, i;
+    for(i = 0; i < n; ++i){
+        if(i > 0 && nums[i] == nums[i-1]){
+            ++count;
+            continue;
+        }else{
+            if(count > n / 3) result.push_back(nums[i-1]);
+            count = 1;
+        }
+    }
+    if(count > n / 3) result.push_back(nums[i-1]);
+    return result;
+}
