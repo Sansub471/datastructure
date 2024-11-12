df5ec535b1deb1ed83a9ac301214a747d79958b5
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-11-12 15:57:01 +0545

    Majority element with linear complexity.

--- DSA/LeetProblems/majorityElement.hpp
+++ DSA/LeetProblems/majorityElement.hpp
@@ -2,39 +2,94 @@
 #include<vector>
 #include<algorithm>
 int majorityElement(std::vector<int>& nums) {
-    int n = nums.size();
-    std::sort(nums.begin(), nums.end());
-    int count = 0, i;
-    for(i = 0; i < n; ++i){
-        if(i > 0 && nums[i] == nums[i - 1]){
+    // int n = nums.size();
+    // std::sort(nums.begin(), nums.end());
+    // int count = 0;
+    // int i;
+    // for(i = 0; i < n; ++i){
+    //     if(i > 0 && nums[i] == nums[i - 1]){
+    //         ++count;
+    //         continue;
+    //     }else{
+    //         if(count > n/2) return nums[i-1];
+    //         count = 1;
+    //     }
+    // }
+    // if(count > n/2) return nums[i-1];
+    // // for compiler
+    // return -1;
+
+    // For linear approach Boyer-Moore voting algorithm
+    // majority element can cancel out non majority element and still be left with majority element
+    int count = 0;
+    int candidate = nums[0];
+
+    //1. Find the candidate
+    for(int num : nums){
+        if(count == 0){
+            candidate = num;
+        }
+
+        count += (num == candidate) ? 1 : -1;
+    }
+    //2. Optional step, verify the candidate
+    count = 0;
+    for(int num : nums){
+        if(num == candidate){
             ++count;
-            continue;
-        }else{
-            if(count > n/2) return nums[i-1];
-            count = 1;
         }
     }
-    if(count > n/2) return nums[i-1];
-    // for compiler
-    return -1;
+    return count > nums.size() / 2 ? candidate : -1;
+
+    // O(N) as well as O(1)
 }
 
 // Sorting takes O(NlogN) where as after sorting only O(N) hence sorting time is the time complexity
 
 std::vector<int> majorityElement2(std::vector<int>& nums) {
+    // int n = nums.size();
+    // std::sort(nums.begin(), nums.end());
+    // std::vector<int> result;
+    // int count = 0, i;
+    // for(i = 0; i < n; ++i){
+    //     if(i > 0 && nums[i] == nums[i-1]){
+    //         ++count;
+    //         continue;
+    //     }else{
+    //         if(count > n / 3) result.push_back(nums[i-1]);
+    //         count = 1;
+    //     }
+    // }
+    // if(count > n / 3) result.push_back(nums[i-1]);
+    // return result;
+
+    // Liner complexity 
+    int candidate1 =0, candidate2 = 1;
+    int count1 = 0, count2 = 0;
+    vector<int> result;
     int n = nums.size();
-    sort(nums.begin(), nums.end());
-    std::vector<int> result;
-    int count = 0, i;
-    for(i = 0; i < n; ++i){
-        if(i > 0 && nums[i] == nums[i-1]){
-            ++count;
-            continue;
+    for(int num: nums){
+        if(num == candidate1){
+            ++count1;
+        }else if(num == candidate2){
+            ++count2;
+        }else if(count1 == 0){
+            candidate1 = num;
+            count1 = 1;
+        }else if(count2 == 0){
+            candidate2 = num;
+            count2 = 1;
         }else{
-            if(count > n / 3) result.push_back(nums[i-1]);
-            count = 1;
+            --count1; 
+            --count2;
         }
     }
-    if(count > n / 3) result.push_back(nums[i-1]);
+    count1 = count2 = 0;
+    for(int num : nums){
+        if(num == candidate1) ++count1;
+        if(num == candidate2) ++count2;
+    }
+    if(count1 > n / 3) result.push_back(candidate1);
+    if(count2 > n / 3) result.push_back(candidate2);
     return result;
 }
