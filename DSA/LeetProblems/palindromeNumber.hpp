9603a05535c0ebe41c84b34cb8c74243472613a9
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-03 23:54:55 +0545

    check if a number is palindrome or not?

--- DSA/LeetProblems/palindromeNumber.hpp
+++ DSA/LeetProblems/palindromeNumber.hpp
@@ -1,4 +1,10 @@
 #pragma once
-bool palindromeNumber(int N){
-    
+bool isPalindrome(int x) {
+    if(x < 0 || (x % 10 == 0 && x !=0)) return false; // negative and number ending with zero
+    int revhalf = 0;
+    while(x > revhalf){
+        revhalf = revhalf * 10 + x % 10;
+        x /= 10;
+    }
+    return x == revhalf || x == revhalf/10;
 }
