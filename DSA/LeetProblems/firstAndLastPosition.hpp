ea9abeaae5291a4e1fb36e721d255bad2a1f8bc8
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-15 23:34:27 +0545

    Problem description and conditions are added.

--- DSA/LeetProblems/firstAndLastPosition.hpp
+++ DSA/LeetProblems/firstAndLastPosition.hpp
@@ -1,6 +1,13 @@
 #pragma once
 #include<vector>
 
+// Given an array of integers nums sorted in non-decreasing order, 
+// find the starting and ending position of a given target value.
+
+// If target is not found in the array, return [-1, -1].
+
+// You must write an algorithm with O(log n) runtime complexity.
+
 std::vector<int> searchRange(std::vector<int>& nums, int target) {
     if(nums.empty()) return {-1, -1};
     int low = 0, high = nums.size() - 1;
