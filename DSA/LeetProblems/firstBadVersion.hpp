282c51f7e89feed37faf94fc455a849bce67ea7e
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-16 22:14:09 +0545

    First bad version code works.

--- DSA/LeetProblems/firstBadVersion.hpp
+++ DSA/LeetProblems/firstBadVersion.hpp
@@ -1,3 +1,23 @@
 #pragma once
 
 // 278. First Bad Version
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+bool isBadVersion(int n){} // Dummy to prevent compiler complain
+
+int firstBadVersion(int n) {
+    int low = 1, high = n;
+    int pos = -1;
+    while(low <= high ){
+        int mid = low + (high - low + 1) / 2; // +1 adds bias towards high, helps faster convergence sometimes
+        bool badChk = isBadVersion(mid);
+        if(badChk){
+            high = mid - 1;
+            pos = mid;
+        }else{
+            low = mid + 1;
+        }
+    }
+    return pos;
+}
