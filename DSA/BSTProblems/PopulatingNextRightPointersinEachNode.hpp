36fc2b7fa2b678a4914bfd275b9366c8b81ffc66
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-17 23:44:36 +0545

    The problem is solved, but the code is slow.

--- DSA/BSTProblems/PopulatingNextRightPointersinEachNode.hpp
+++ DSA/BSTProblems/PopulatingNextRightPointersinEachNode.hpp
@@ -14,3 +14,42 @@
 // should be set to NULL.
 
 // Initially, all next pointers are set to NULL.
+
+// Definition for a Node.
+
+#include<queue>
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val) : val(_val), left(nullptr), right(nullptr), next(nullptr) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+
+Node* connect(Node* root) {
+    if(root == nullptr) return root;
+    std::queue<Node*> Q;
+    Q.push(root);
+    while(!Q.empty()){
+        int SIZE = Q.size();
+        Node* prevFront = nullptr;
+        for(int i = 0; i < SIZE; i++){
+            Node* currFront = Q.front();
+            currFront->next = prevFront;
+            Q.pop();
+            if(currFront->right) Q.push(currFront->right);
+            if(currFront->left) Q.push(currFront->left);
+            prevFront = currFront;
+        }
+        //prevFront->next = nullptr;
+    }
+    return root;
+}
