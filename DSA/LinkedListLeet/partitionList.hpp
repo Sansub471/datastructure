360c688e38ab036962722e22e176367215bf5abe
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-22 23:59:19 +0545

    The code is correct now.

--- DSA/LinkedListLeet/partitionList.hpp
+++ DSA/LinkedListLeet/partitionList.hpp
@@ -19,42 +19,30 @@ typedef LinkedList::Node ListNode;
 ListNode* partition(ListNode* head, int x) {
     if(head == nullptr || head->next == nullptr) return head;
     ListNode* lesser_dummy = new ListNode(0, nullptr);
-    ListNode* lesser_ptr = nullptr;
+    ListNode* lesser_ptr = lesser_dummy;
 
     ListNode* greaterEqual_dummy = new ListNode(0, nullptr);
-    ListNode* greaterEqual_ptr = nullptr;
+    ListNode* greaterEqual_ptr = greaterEqual_dummy;
 
     ListNode* headptr = head;
-    while(headptr->next != nullptr){
+    while(headptr != nullptr){
         if(headptr->val < x){
-            if(lesser_dummy->next == nullptr){
-                lesser_dummy->next = headptr;
-                lesser_ptr = headptr;
-                lesser_ptr->next = nullptr;
-            }
             lesser_ptr->next = headptr;
             lesser_ptr = headptr;
-            lesser_ptr->next = nullptr;
         }else{
-            if(greaterEqual_dummy->next == nullptr){
-                greaterEqual_dummy->next = headptr;
-                greaterEqual_ptr = headptr;
-                greaterEqual_ptr->next = nullptr;
-
-            }
             greaterEqual_ptr->next = headptr;
             greaterEqual_ptr = headptr;
-            greaterEqual_ptr->next = nullptr;
         }
         headptr = headptr->next;
     }
+
+    greaterEqual_ptr->next = nullptr; // terminate the greater equal list
 
     // join the two lists
     lesser_ptr->next = greaterEqual_dummy->next;
     delete greaterEqual_dummy;
     
-    ListNode* temp = lesser_dummy;
-    lesser_dummy = lesser_dummy->next;
-    delete temp;
-    return lesser_dummy;
+    ListNode* new_head = lesser_dummy->next;
+    delete lesser_dummy;
+    return new_head;
 }
