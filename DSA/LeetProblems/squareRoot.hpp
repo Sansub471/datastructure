27b3dc7dcf2f99a2052d2f435e3bf7315b5caa6d
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-25 00:10:53 +0545

    The sqaure root without using the built in functions.

--- DSA/LeetProblems/squareRoot.hpp
+++ DSA/LeetProblems/squareRoot.hpp
@@ -1,3 +1,22 @@
 #pragma once
 
 // Square root without using the built in funciton
+int mySqrt(int x) {
+    if(x == 0 || x == 1) return x;
+    int low = 1, high = x / 2; // for x > 1 it can be x/2
+    int potential = 0;
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+        int temp = x / mid;
+        if(mid == temp) return mid;
+        else if(mid < temp){
+            low = mid + 1;
+            potential = mid;
+        }else{
+            high = mid - 1;
+        }
+    }
+    return potential;
+
+    // O(log x ) complexity
+}
