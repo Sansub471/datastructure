26dfb5990a9c6c0f8dfdf626042242307c01d4cc
Sansub471 <075bei039.subash@pcampus.edu.np>
2024-12-25 00:13:51 +0545

    Can return the high instead of potential value too.

--- DSA/LeetProblems/squareRoot.hpp
+++ DSA/LeetProblems/squareRoot.hpp
@@ -4,19 +4,17 @@
 int mySqrt(int x) {
     if(x == 0 || x == 1) return x;
     int low = 1, high = x / 2; // for x > 1 it can be x/2
-    int potential = 0;
     while(low <= high){
         int mid = low + (high - low) / 2;
         int temp = x / mid;
         if(mid == temp) return mid;
         else if(mid < temp){
             low = mid + 1;
-            potential = mid;
         }else{
             high = mid - 1;
         }
     }
-    return potential;
+    return high;
 
     // O(log x ) complexity
 }
