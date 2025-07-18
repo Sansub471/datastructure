b767ddb85566ee632db46aaafc44297e613ceb70
Sansub471 <075bei039.subash@pcampus.edu.np>
2025-07-18 23:03:33 +0545

    The day has come for me.

--- DSA/m-wayTree/mTreemain.cpp
+++ DSA/m-wayTree/mTreemain.cpp
@@ -3,5 +3,7 @@
 int main(){
 
     std::cout<<"This is the file for m-way tree allocation..."<<std::endl;
+
+    std::cout<<"This is not my first day for the game, it is not made it right for me."<<std::endl;
     return 0;
 }
