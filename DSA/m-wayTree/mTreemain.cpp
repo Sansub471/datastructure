6d8066ba414bc187912cc3b8d03b124fbcb8a2e4
Sansub471 <075bei039.subash@pcampus.edu.np>
2025-07-18 23:05:22 +0545

    The name of the game is right.

--- DSA/m-wayTree/mTreemain.cpp
+++ DSA/m-wayTree/mTreemain.cpp
@@ -3,7 +3,12 @@
 int main(){
 
     std::cout<<"This is the file for m-way tree allocation..."<<std::endl;
-
+    std::cout<<"The name of the game is right for me."<<std::endl;
     std::cout<<"This is not my first day for the game, it is not made it right for me."<<std::endl;
+
+    std::cout<<"The name of the game is right for me."<<std::endl;
+
+    std::cout<<"The name of the game is right for me is right. The name of the game is right is right for me."<<std::endl;
+
     return 0;
 }
