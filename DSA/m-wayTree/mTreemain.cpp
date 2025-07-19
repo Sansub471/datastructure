338e37dcebd2845c73402bed8001d4eb6651f407
Sansub471 <075bei039.subash@pcampus.edu.np>
2025-07-19 21:52:48 +0545

    Print statement removed.

--- DSA/m-wayTree/mTreemain.cpp
+++ DSA/m-wayTree/mTreemain.cpp
@@ -6,9 +6,7 @@ int main(){
     std::cout<<"The name of the game is right for me."<<std::endl;
     std::cout<<"This is not my first day for the game, it is not made it right for me."<<std::endl;
 
-    std::cout<<"The name of the game is right for me."<<std::endl;
-
-    std::cout<<"The name of the game is right for me is right. The name of the game is right is right for me."<<std::endl;
+   
 
     return 0;
 }
