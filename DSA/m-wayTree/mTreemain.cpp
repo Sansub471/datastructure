253bc71854647bececa4d6f983bc7b2b0d57842a
Sansub471 <075bei039.subash@pcampus.edu.np>
2025-07-19 21:53:05 +0545

    Print statement removed.

--- DSA/m-wayTree/mTreemain.cpp
+++ DSA/m-wayTree/mTreemain.cpp
@@ -2,9 +2,7 @@
 
 int main(){
 
-    std::cout<<"This is the file for m-way tree allocation..."<<std::endl;
-    std::cout<<"The name of the game is right for me."<<std::endl;
-    std::cout<<"This is not my first day for the game, it is not made it right for me."<<std::endl;
+  
 
    
 
