bf0cd91786a7727d33fc19b2242dda15d10b21d7
Sansub471 <075bei039.subash@pcampus.edu.np>
2025-08-02 21:28:38 +0545

    The code works, let's continue.

--- DSA/m-wayTree/mTreemain.cpp
+++ DSA/m-wayTree/mTreemain.cpp
@@ -1,13 +1,9 @@
 #include"mTree.hpp"
 
-int main() {
-    const int M = 4;
-    MWayTree<int, M> T;
-    int arr[] = {10, 20, 5, 15, 25, 12};
-    for (int x : arr) T.insertAndShow(x);
 
-    cout << "Search 15: " << (T.contains(15) ? "found" : "not found") << "\n";
-    cout << "Search 100: " << (T.contains(100) ? "found" : "not found") << "\n";
+int main() {
+    // The user will be able to determine the size of the Node.
+    
     return 0;
 }
 
