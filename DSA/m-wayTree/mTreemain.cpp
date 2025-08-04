89b25a44563ab2d590627ecc0275ca865be2f265
Sansub471 <075bei039.subash@pcampus.edu.np>
2025-08-04 21:33:16 +0545

    The code is running, now let's finish it.

--- DSA/m-wayTree/mTreemain.cpp
+++ DSA/m-wayTree/mTreemain.cpp
@@ -2,7 +2,16 @@
 
 
 int main() {
-    
+    const int M = 4;
+    MWayTree<int, M> m_tree;
+
+    int arr[] = {10, 20, 5, 15, 25, 12};
+    for(int x : arr){
+        m_tree.insert(x);
+    }
+
+    std::cout<<"Search 15: "<< (m_tree.contains(15) ? "Found" : "Not Found") << "\n";
+    std::cout<<"Search 99: "<< (m_tree.contains(99) ? "Found" : "Not Found") << "\n";
     return 0;
 }
 
